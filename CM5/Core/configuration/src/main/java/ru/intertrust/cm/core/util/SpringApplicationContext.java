5f2b93e6528fb7c437c0d2d3b73c3ad8fcf1a1c3
Andrey Pirozhkov <apirozhkov@inttrust.ru>
2013-06-28 19:18:29 +0400

    Добавлена возможность получения Sping-контекста приложения из любого места (временное размещение)

---
+++ CM5/Core/configuration/src/main/java/ru/intertrust/cm/core/util/SpringApplicationContext.java
@@ -0,0 +1,23 @@
+package ru.intertrust.cm.core.util;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import ru.intertrust.cm.core.model.FatalException;
+
+public class SpringApplicationContext implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) {
+        if (context != null) {
+            throw new FatalException("Repeated set of context");
+        }
+        context = ctx;
+    }
+
+    public static ApplicationContext getContext() {
+        return context;
+    }
+}
