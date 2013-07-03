1fc1134de3dc9f43b3c613129e4bdad87a3a15eb
dmitavskiy <dmitavskiy@inttrust.ru>
2013-07-03 18:49:59 +0300

    Интеграция с JAAS.

--- CM5/Core/gui-impl/src/main/java/ru/intertrust/cm/core/client/Hello.java
+++ CM5/Core/gui-impl/src/main/java/ru/intertrust/cm/core/client/Hello.java
@@ -2,12 +2,15 @@ package ru.intertrust.cm.core.client;
 
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinService;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 import ru.intertrust.cm.core.config.model.Configuration;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.StringWriter;
 
@@ -18,14 +21,25 @@ import java.io.StringWriter;
  */
 @Title("hello")
 public class Hello extends UI {
+    protected boolean isLoggedIn(VaadinRequest request) {
+        return request.getWrappedSession().getAttribute("USER_LOGIN") != null;
+    }
+
     @Override
     protected void init(VaadinRequest request) {
+        request.getWrappedSession();
+        final HttpServletRequest currentRequest = (HttpServletRequest) VaadinService.getCurrentRequest();
+        try {
+            currentRequest.login("admin", "admin");
+        } catch (ServletException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
         HorizontalSplitPanel splitPanel = new HorizontalSplitPanel();
         setContent(splitPanel);
 
-        /* Build the component tree */
         VerticalLayout leftLayout = new VerticalLayout();
-        leftLayout.addComponent(new Label(getConfigXml(), ContentMode.PREFORMATTED));
+        leftLayout.addComponent(new Label("Welcome Home!", ContentMode.PREFORMATTED));
         splitPanel.addComponent(leftLayout);
 
         HorizontalLayout bottomLeftLayout = new HorizontalLayout();
@@ -36,6 +50,48 @@ public class Hello extends UI {
         leftLayout.setSizeFull();
 
         bottomLeftLayout.setWidth("100%");
+
+        Window subWindow = new Window("Log in");
+        subWindow.setClosable(false);
+        VerticalLayout subContent = new VerticalLayout();
+        subContent.setMargin(true);
+        subWindow.setContent(subContent);
+
+        // Put some components in it
+        //subContent.addComponent(new Label("Login"));
+        final Label message = new Label("");
+        final TextField loginField = new TextField("Login");
+        final PasswordField passwordField = new PasswordField("Password");
+        Button loginButton = new Button("Login");
+        loginButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                String login = loginField.getValue();
+                String password = passwordField.getValue();
+                try {
+                    currentRequest.login(login, password);
+                    message.setValue("Success!");
+                } catch (ServletException e) {
+                    message.setValue(e.getMessage());
+                }
+            }
+        });
+        subContent.addComponent(message);
+        subContent.addComponent(loginField);
+        subContent.addComponent(passwordField);
+        subContent.addComponent(loginButton);
+
+        // Center it in the browser window
+        subWindow.center();
+
+        // Open it in the UI
+        addWindow(subWindow);
+
+        try {
+            currentRequest.logout();
+        } catch (ServletException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
     public static String getConfigXml() {
