2cf9731a3d69460b8001ef0d2a7d8fdac6a13c6a
Ilya Tugushev <itugushev@inttrust.ru>
2013-07-17 12:00:57 +0400

    Фиктивный класс в модуле Workstation

---
+++ CM5/Workstation/src/main/java/ru/intertrust/cm/ws/App.java
@@ -0,0 +1,14 @@
+package ru.intertrust.cm.ws;
+
+/**
+ * Некоторые утилиты сборки (типа FindBug запускаемого через sonar),
+ * требуют наличия хоть какого-то кода в модуле.
+ * Для этого данный класс и создан.
+ * Удалить после появления любого другого класса в данном модуле
+ * @author ITugushev
+ **/
+ public class App {
+    public static void main(String[] args) {
+
+    }
+}
