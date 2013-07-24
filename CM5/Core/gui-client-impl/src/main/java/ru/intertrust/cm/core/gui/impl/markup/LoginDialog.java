cfd3e0516ce679e90cd203c8ef0ceb9650cc92ff
dmitavskiy <dmitavskiy@inttrust.ru>
2013-07-24 14:40:28 +0300

    CMFIVE-9: Добавлена инфраструктура для размещения разметки

---
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/markup/LoginDialog.java
@@ -0,0 +1,10 @@
+package ru.intertrust.cm.core.gui.impl.markup;
+
+/**
+ * @author Denis Mitavskiy
+ *         Date: 24.07.13
+ *         Time: 13:22
+ */
+public class LoginDialog {
+
+}
---
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/markup/MainView.java
@@ -0,0 +1,16 @@
+package ru.intertrust.cm.core.gui.impl.markup;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+/**
+ * @author Denis Mitavskiy
+ *         Date: 24.07.13
+ *         Time: 13:52
+ */
+public class MainView implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+        Window.alert("Hello");
+    }
+}
