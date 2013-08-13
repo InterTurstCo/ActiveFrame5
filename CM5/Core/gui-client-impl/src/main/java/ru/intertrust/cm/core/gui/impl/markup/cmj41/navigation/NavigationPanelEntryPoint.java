b862bace15f38e7ab56b6d35fb2a76fdd05cad67
dmitavskiy <dmitavskiy@inttrust.ru>
2013-08-13 16:18:27 +0300

    CMFIVE-55: инфраструктура вёрстки CMJ4

---
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/markup/cmj41/navigation/NavigationPanelEntryPoint.java
@@ -0,0 +1,15 @@
+package ru.intertrust.cm.core.gui.impl.markup.cmj41.navigation;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author Denis Mitavskiy
+ *         Date: 13.08.13
+ *         Time: 15:19
+ */
+public class NavigationPanelEntryPoint implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+
+    }
+}
