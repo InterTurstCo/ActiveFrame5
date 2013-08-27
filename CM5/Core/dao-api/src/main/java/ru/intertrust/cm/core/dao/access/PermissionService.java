30b0dce6574d511eeae83de5d05840691728a892
atsvetkov <andrey.tsvetkov@augmentive.com>
2013-08-27 16:17:56 +0300

    CMFIVE-30. Реализация сервиса пересчета списков доступа. Коментарии.

--- CM5/Core/dao-api/src/main/java/ru/intertrust/cm/core/dao/access/PermissionService.java
+++ CM5/Core/dao-api/src/main/java/ru/intertrust/cm/core/dao/access/PermissionService.java
@@ -10,14 +10,15 @@ import ru.intertrust.cm.core.business.api.dto.Id;
 public interface PermissionService {
 
     /**
-     * Пересчитывает списки доступа (таблицы Object_ACL, Object_READ) для доменного объекта.
-     * @param objectId идентификатор доменного объекта
+     * Пересчитывает списки доступа (таблицы Object_ACL, Object_READ) для доменного объекта. Сначала удаляет все списки
+     * доступа для доменного объекта, потом персчитывает заново.
+     * @param objectId идентификатор доменного объекта, для которого персчитываются списки доступа
      */
     void refreshAclFor(Id objectId);
     
     /**
      * Удаляет списки доступа (таблицы Object_ACL, Object_READ) для доменного объекта.
-     * @param objectId
+     * @param objectId идентификатор доменного объекта, для которого удаляются списки доступа
      */
     void deleteAclFor(Id objectId);
 }
