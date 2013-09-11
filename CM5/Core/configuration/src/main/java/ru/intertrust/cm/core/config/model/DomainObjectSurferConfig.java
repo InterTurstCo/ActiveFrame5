cb3c5238e60a73725f634590ed06ce8b5e1e2ab7
ybondarchuk <yaroslav.i.bondarchuk@gmail.com>
2013-09-11 17:38:12 +0300

    Пустой Configuration для  NavigationConfig

--- CM5/Core/configuration/src/main/java/ru/intertrust/cm/core/config/model/DomainObjectSurferConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.intertrust.cm.core.config.model;
-
-/**
- * Author: Denis Mitavskiy
- * Date: 14.06.13
- * Time: 16:04
- */
-public class DomainObjectSurferConfig extends PluginImplConfig {
-}
