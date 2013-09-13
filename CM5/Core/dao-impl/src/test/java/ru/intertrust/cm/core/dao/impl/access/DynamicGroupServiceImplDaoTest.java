f50afbf8f9de48e7c5ac30affc8e516d51fea2e0
atsvetkov <andrey.tsvetkov@augmentive.com>
2013-09-13 14:39:03 +0300

    CMFIVE-30. Закоментирован DynamicGroupServiceImplDaoTest.

--- CM5/Core/dao-impl/src/test/java/ru/intertrust/cm/core/dao/impl/access/DynamicGroupServiceImplDaoTest.java
+++ CM5/Core/dao-impl/src/test/java/ru/intertrust/cm/core/dao/impl/access/DynamicGroupServiceImplDaoTest.java
@@ -3,7 +3,7 @@ package ru.intertrust.cm.core.dao.impl.access;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class DynamicGroupServiceImplDaoTest {
 
 }
