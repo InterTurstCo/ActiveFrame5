0d86b8e0435b6c3034cb7b8b702823294d3ca5a9
dmitavskiy <dmitavskiy@inttrust.ru>
2013-09-15 21:31:46 +0300

    CMFIVE-80: фикс сборки - закомментирован перехватчик

--- CM5/Core/service-impl/src/main/java/ru/intertrust/cm/core/business/impl/CrudServiceImpl.java
+++ CM5/Core/service-impl/src/main/java/ru/intertrust/cm/core/business/impl/CrudServiceImpl.java
@@ -14,8 +14,6 @@ import ru.intertrust.cm.core.dao.api.DomainObjectDao;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
-import javax.interceptor.Interceptors;
-
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -28,7 +26,7 @@ import java.util.List;
 @Stateless
 @Local(CrudService.class)
 @Remote(CrudService.Remote.class)
-@Interceptors(EjbAutowiringInterceptor.class)
+//@Interceptors(EjbAutowiringInterceptor.class)
 public class CrudServiceImpl implements CrudService, CrudService.Remote {
 
     @Autowired
