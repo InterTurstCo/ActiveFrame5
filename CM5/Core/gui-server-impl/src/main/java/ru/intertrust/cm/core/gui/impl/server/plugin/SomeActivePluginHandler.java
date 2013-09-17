dfc5e325718b0b444c3af2a9324e059e9d085740
akarandey <andrey.n.karandey@gmail.com>
2013-09-17 13:02:04 +0300

    CMFIVE-72: Смерджил SomeActivePluginHandler

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/plugin/SomeActivePluginHandler.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/plugin/SomeActivePluginHandler.java
@@ -1,13 +1,18 @@
 package ru.intertrust.cm.core.gui.impl.server.plugin;
 
 import ru.intertrust.cm.core.business.api.dto.Dto;
+import ru.intertrust.cm.core.gui.api.server.GuiService;
 import ru.intertrust.cm.core.gui.api.server.plugin.ActivePluginHandler;
 import ru.intertrust.cm.core.gui.model.ActionConfig;
 import ru.intertrust.cm.core.gui.model.ComponentName;
+import ru.intertrust.cm.core.gui.model.GuiException;
 import ru.intertrust.cm.core.gui.model.plugin.ActivePluginData;
 import ru.intertrust.cm.core.gui.model.plugin.SomeActivePluginData;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Denis Mitavskiy
@@ -18,21 +23,31 @@ import java.util.ArrayList;
 public class SomeActivePluginHandler extends ActivePluginHandler {
     @Override
     public ActivePluginData initialize(Dto param) {
+        SomeActivePluginData pluginData = new SomeActivePluginData();
+        pluginData.setActionConfigs(getActions());
+        pluginData.setForm(getGuiService().getForm(null));
         System.out.println("SomeActivePluginHandler initialized!");
-        return null;
+        return pluginData;
     }
 
-    @Override
-    public ActivePluginData createPluginData() {
-        SomeActivePluginData pluginData = new SomeActivePluginData();
-        ArrayList<ActionConfig> actionConfigs = new ArrayList<>();
-        actionConfigs.add(new ActionConfig("Action 1"));
-        pluginData.setActionConfigs(actionConfigs);
-        return pluginData;
+    public List<ActionConfig> getActions()  {
+        ArrayList<ActionConfig> actions = new ArrayList<>();
+        actions.add(new ActionConfig("Сохранить"));
+        return actions;
     }
 
     public ActivePluginData doSomethingVeryGood(Dto dto) {
         System.out.println("SomeActivePluginHandler executed doSomethingVeryGood()");
         return null;
+    }
+
+    private GuiService getGuiService() {
+        InitialContext ctx;
+        try {
+            ctx = new InitialContext();
+            return (GuiService) ctx.lookup("java:app/web-app/GuiServiceImpl!ru.intertrust.cm.core.gui.api.server.GuiService");
+        } catch (NamingException ex) {
+            throw new GuiException("EJB not found", ex);
+        }
     }
 }
