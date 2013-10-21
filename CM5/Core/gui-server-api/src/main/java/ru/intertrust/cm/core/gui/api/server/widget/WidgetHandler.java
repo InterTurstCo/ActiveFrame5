73238d535ff546b6831ba187d606f7d7575337e4
dmitavskiy <dmitavskiy@inttrust.ru>
2013-10-21 03:06:20 +0300

    CMFIVE-76: Поддержка виджетов, меняющих связи 1:N и N:M

--- CM5/Core/gui-server-api/src/main/java/ru/intertrust/cm/core/gui/api/server/widget/WidgetHandler.java
+++ CM5/Core/gui-server-api/src/main/java/ru/intertrust/cm/core/gui/api/server/widget/WidgetHandler.java
@@ -1,8 +1,15 @@
 package ru.intertrust.cm.core.gui.api.server.widget;
 
+import ru.intertrust.cm.core.business.api.dto.*;
 import ru.intertrust.cm.core.gui.api.server.ComponentHandler;
 import ru.intertrust.cm.core.gui.model.form.widget.WidgetContext;
 import ru.intertrust.cm.core.gui.model.form.widget.WidgetState;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Denis Mitavskiy
@@ -11,4 +18,47 @@ import ru.intertrust.cm.core.gui.model.form.widget.WidgetState;
  */
 public abstract class WidgetHandler implements ComponentHandler {
     public abstract <T extends WidgetState> T getInitialState(WidgetContext context);
+
+    protected ArrayList<String> format(List<DomainObject> listToDisplay, String displayPattern) {
+        Pattern pattern = Pattern.compile("\\{\\w+\\}");
+        Matcher matcher = pattern.matcher(displayPattern);
+        ArrayList<String> displayValues = new ArrayList<>(listToDisplay.size());
+        for (DomainObject domainObject : listToDisplay) {
+            displayValues.add(format(domainObject, matcher));
+        }
+        return displayValues;
+    }
+
+    private String format(DomainObject domainObject, Matcher matcher) {
+        StringBuffer replacement = new StringBuffer();
+        while (matcher.find()) {
+            String group = matcher.group();
+            String fieldName = group.substring(1, group.length() - 1);
+            Value value = domainObject.getValue(fieldName);
+            String displayValue = "";
+            if (value != null) {
+                Object primitiveValue = value.get();
+                if (primitiveValue == null) {
+                    if (value instanceof LongValue || value instanceof DecimalValue) {
+                        displayValue = "0";
+                    }
+                } else {
+                    displayValue = primitiveValue.toString();
+                }
+            }
+            matcher.appendReplacement(replacement, displayValue);
+        }
+        matcher.appendTail(replacement);
+        matcher.reset();
+        return replacement.toString();
+    }
+
+    protected void appendDisplayMappings(List<DomainObject> listToDisplay, String displayPattern,
+                                         LinkedHashMap<Id, String> idDisplayMapping) {
+        ArrayList<String> displayValues = format(listToDisplay, displayPattern);
+        for (int i = 0; i < listToDisplay.size(); i++) {
+            DomainObject domainObject = listToDisplay.get(i);
+            idDisplayMapping.put(domainObject.getId(), displayValues.get(i));
+        }
+    }
 }
--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/widget/ListBoxHandler.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/widget/ListBoxHandler.java
@@ -1,9 +1,9 @@
 package ru.intertrust.cm.core.gui.impl.server.widget;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import ru.intertrust.cm.core.business.api.ConfigurationService;
 import ru.intertrust.cm.core.business.api.CrudService;
-import ru.intertrust.cm.core.business.api.dto.*;
+import ru.intertrust.cm.core.business.api.dto.DomainObject;
+import ru.intertrust.cm.core.business.api.dto.Id;
 import ru.intertrust.cm.core.config.model.gui.form.widget.ListBoxConfig;
 import ru.intertrust.cm.core.gui.api.server.widget.MultiObjectWidgetHandler;
 import ru.intertrust.cm.core.gui.model.ComponentName;
@@ -14,8 +14,6 @@ import ru.intertrust.cm.core.gui.model.form.widget.WidgetContext;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Denis Mitavskiy
@@ -26,22 +24,15 @@ import java.util.regex.Pattern;
 public class ListBoxHandler extends MultiObjectWidgetHandler {
     @Autowired
     protected CrudService crudService;
-    @Autowired
-    protected ConfigurationService configurationService;
 
     @Override
     public ListBoxState getInitialState(WidgetContext context) {
         ListBoxConfig widgetConfig = context.getWidgetConfig();
 
         FieldPath fieldPath = new FieldPath(widgetConfig.getFieldPathConfig().getValue());
-        ArrayList<DomainObject> objectReferencingField = context.getFormObjects().getObjects(fieldPath.getParent()).getDomainObjects(); // todo we can't find type when DO is empty, thus - fix. we got to push type from outside
-        Id baseObjectId = objectReferencingField.get(0).getId();
+        String linkType = getLinkedObjectType(context, fieldPath);
 
-        String field = fieldPath.getLastElement();
-        String[] referencingTypeAndField = field.split("\\^");
-        String referencingType = referencingTypeAndField[0];
-        String referencingField = referencingTypeAndField[1];
-        List<DomainObject> listToDisplay = crudService.findAll(referencingType);
+        List<DomainObject> listToDisplay = crudService.findAll(linkType);
         LinkedHashMap<Id, String> idDisplayMapping = new LinkedHashMap<>();
 
         ListBoxState result = new ListBoxState();
@@ -51,50 +42,13 @@ public class ListBoxHandler extends MultiObjectWidgetHandler {
             return result;
         }
 
-        Pattern pattern = Pattern.compile("\\{\\w+\\}");
-        Matcher matcher = pattern.matcher(widgetConfig.getPatternConfig().getValue());
-        for (DomainObject domainObject : listToDisplay) {
-            String format = format(domainObject, matcher);
-            idDisplayMapping.put(domainObject.getId(), format);
-        }
+        String displayPattern = widgetConfig.getPatternConfig().getValue();
+        appendDisplayMappings(listToDisplay, displayPattern, idDisplayMapping);
 
-        if (baseObjectId == null) {
-            return result;
-        }
-        ArrayList<Id> selectedIds = new ArrayList<>();
-        for (DomainObject domainObject : listToDisplay) {
-            Id referenceToBaseObject = domainObject.getReference(referencingField);
-            if (baseObjectId.equals(referenceToBaseObject)) {
-                selectedIds.add(domainObject.getId());
-            }
-        }
-
+        ArrayList<Id> selectedIds = context.getFormObjects().getObjectIds(fieldPath);
         result.setSelectedIds(selectedIds);
 
         return result;
     }
 
-    private String format(DomainObject domainObject, Matcher matcher) {
-        StringBuffer replacement = new StringBuffer();
-        while (matcher.find()) {
-            String group = matcher.group();
-            String fieldName = group.substring(1, group.length() - 1);
-            Value value = domainObject.getValue(fieldName);
-            String displayValue = "";
-            if (value != null) {
-                Object primitiveValue = value.get();
-                if (primitiveValue == null) {
-                    if (value instanceof LongValue || value instanceof DecimalValue) {
-                        displayValue = "0";
-                    }
-                } else {
-                    displayValue = primitiveValue.toString();
-                }
-            }
-            matcher.appendReplacement(replacement, displayValue);
-        }
-        matcher.appendTail(replacement);
-        matcher.reset();
-        return replacement.toString();
-    }
 }
