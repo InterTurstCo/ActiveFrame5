ad648bd68828294ab130ae9970cee1fe48845175
akarandey <andrey.n.karandey@gmail.com>
2013-11-05 11:27:51 +0200

    Добавлена верстка окна выбора адрессата

--- CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/markup/MainView.java
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/markup/MainView.java
@@ -3,8 +3,9 @@ package ru.intertrust.cm.core.gui.impl.markup;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.ui.*;
 
 /**
  * @author Denis Mitavskiy Date: 24.07.13 Time: 13:52
@@ -12,7 +13,7 @@ import com.google.gwt.user.client.ui.RootLayoutPanel;
 public class MainView implements EntryPoint {
     @Override
     public void onModuleLoad() {
-        // Window.alert("Hello");
+      /*  // Window.alert("Hello");
 
         final Mainform mf = new Mainform();
 
@@ -47,10 +48,121 @@ public class MainView implements EntryPoint {
         // mf.addElementInNavigation(new Button("navigation"));
         // mf.addElementInTree(treeBtn);
         mf.addElementInSticker(stickerBtn);
-
-
         RootLayoutPanel rp = RootLayoutPanel.get();
-        rp.add(mf);
+        rp.add(mf);*/
 
+
+        final ChooseAddresserDialog addresserDialog = new ChooseAddresserDialog();
+        addresserDialog.setAnimationEnabled(true);
+        Tree addresserTree = new Tree();
+        VerticalPanel dialogContent = new VerticalPanel();
+        dialogContent.setBorderWidth(1);
+        dialogContent.setWidth("100%");
+        dialogContent.setHeight("100%");
+
+        addresserDialog.setText("Адресат");
+
+
+        SimplePanel treeContainer = new SimplePanel();
+        final TreeItem rootItem = new TreeItem();
+        CheckBoxTreeItemAware rootCheckbox = new CheckBoxTreeItemAware("RootItem", rootItem);
+
+        ValueChangeHandler<Boolean> itemSelectionHandler = new ValueChangeHandler<Boolean>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<Boolean> event) {
+                CheckBoxTreeItemAware source = (CheckBoxTreeItemAware) event.getSource();
+                TreeItem treeItem = source.getTreeItem();
+                for (int i = 0; i < treeItem.getChildCount(); i++) {
+                    CheckBox widget = (CheckBox) treeItem.getChild(i).getWidget();
+                    widget.setValue(event.getValue());
+                }
+            }
+        };
+        rootCheckbox.addValueChangeHandler(itemSelectionHandler);
+        rootItem.setWidget(rootCheckbox);
+
+        for (int i = 0; i < 3; i++) {
+            TreeItem adresser = new TreeItem();
+            CheckBoxTreeItemAware adresserCheckbox = new CheckBoxTreeItemAware("adresser" + i, adresser);
+            adresserCheckbox.addValueChangeHandler(itemSelectionHandler);
+            adresser.setWidget(adresserCheckbox);
+            rootItem.addItem(adresser);
+        }
+
+        addresserTree.addItem(rootItem);
+        addresserDialog.setTitle("title");
+
+        HorizontalPanel buttons = new HorizontalPanel();
+        buttons.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+
+        Button expandAllButton = new Button("Развернуть все");
+        expandAllButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                changeTreeState(rootItem, true);
+            }
+        });
+        buttons.add(expandAllButton);
+        Button collapseAllButton = new Button("Свернуть все");
+        collapseAllButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                changeTreeState(rootItem, false);
+            }
+        });
+        buttons.add(collapseAllButton);
+        dialogContent.add(buttons);
+
+        treeContainer.add(addresserTree);
+        dialogContent.add(new ScrollPanel(treeContainer));
+        Button closeButton = new Button();
+        closeButton.setText("Отмена");
+
+        HorizontalPanel bottomButtons = new HorizontalPanel();
+        bottomButtons.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+        bottomButtons.add(new Button("Добавить адресата"));
+        bottomButtons.add(new Button("Добавить"));
+        bottomButtons.add(closeButton);
+
+        dialogContent.add(bottomButtons);
+
+        addresserDialog.setWidget(dialogContent);
+        addresserDialog.setGlassEnabled(true);
+        addresserDialog.setHeight("300px");
+        addresserDialog.setWidth("400px");
+
+        closeButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                addresserDialog.hide();
+            }
+        });
+        addresserDialog.center();
+    }
+
+    private void changeTreeState(TreeItem tree, boolean state) {
+        tree.setState(state, false);
+        for (int i = 0; i < tree.getChildCount(); i++) {
+            TreeItem child = tree.getChild(i);
+            child.setState(false, false);
+        }
+    }
+
+    class ChooseAddresserDialog extends DialogBox {
+
+
+    }
+
+    class CheckBoxTreeItemAware extends CheckBox {
+        TreeItem treeItem;
+
+        CheckBoxTreeItemAware(String label, TreeItem treeItem) {
+            super(label);
+            this.treeItem = treeItem;
+        }
+
+        TreeItem getTreeItem() {
+            return treeItem;
+        }
     }
 }
