a1f834ee79bf87b5f87f8432073679bca970b604
Andrey Pirozhkov <apirozhkov@inttrust.ru>
2013-12-02 14:58:46 +0400

    CMFIVE-218: Перенос классов конфигурационной модели в проект model

--- CM5/Core/model/src/main/java/ru/intertrust/cm/core/config/search/SearchAreaConfig.java
+++ CM5/Core/model/src/main/java/ru/intertrust/cm/core/config/search/SearchAreaConfig.java
@@ -1,8 +1,8 @@
-package ru.intertrust.cm.core.config.model.search;
+package ru.intertrust.cm.core.config.search;
 
 import org.simpleframework.xml.Root;
 
-import ru.intertrust.cm.core.config.model.base.TopLevelConfig;
+import ru.intertrust.cm.core.config.base.TopLevelConfig;
 
 @Root(name = "search-area")
 public class SearchAreaConfig implements TopLevelConfig {
