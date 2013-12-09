00933d140b9e6edb60d55cc0e169f0eb7184ebf2
ipetrov <igor.gn.petrov@gmail.com>
2013-12-09 15:49:51 +0200

    CMFIVE-210: После выполнения действия обновитьформу/коллекцию основываясь на конфигурации

---
+++ CM5/Core/gui-model/src/main/java/ru/intertrust/cm/core/gui/model/action/DeleteActionData.java
@@ -0,0 +1,12 @@
+package ru.intertrust.cm.core.gui.model.action;
+
+/**
+ * User: IPetrov
+ * Date: 09.12.13
+ * Time: 15:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeleteActionData extends ActionData {
+}
+
+
