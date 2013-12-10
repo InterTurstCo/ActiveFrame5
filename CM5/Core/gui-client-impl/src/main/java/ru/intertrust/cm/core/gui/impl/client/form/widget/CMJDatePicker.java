098f2f37e57672c58aa7e35f0d26e6bed1187ddd
tbilyi <timofiy.s.bilyi@gmail.com>
2013-12-10 12:04:56 +0200

    CMFIVE-261: Перенесено в ядро: DateBox, SuggestBox

--- CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/form/widget/CMJDatePicker.java
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/form/widget/CMJDatePicker.java
@@ -12,17 +12,36 @@ import com.google.gwt.user.datepicker.client.DatePicker;
  * To change this template use File | Settings | File Templates.
  */
 public class CMJDatePicker extends DatePicker {
+      private AbsolutePanel baloon;
+      private AbsolutePanel bottomBaloon;
+      private AbsolutePanel decorateDatePicker;
+
     @Override
     protected void setup() {
-        AbsolutePanel decorateDatePicker = new AbsolutePanel();
+        decorateDatePicker = new AbsolutePanel();
         decorateDatePicker.setStyleName("date-picker-decorate");
-        AbsolutePanel baloon = new AbsolutePanel();
+        baloon = new AbsolutePanel();
         baloon.setStyleName("date-picker-baloon");
         VerticalPanel panel = new VerticalPanel();
         decorateDatePicker.add(panel);
         decorateDatePicker.add(baloon);
+        decorateDatePicker.add(panel);
+        bottomBaloon = new AbsolutePanel();
+        decorateDatePicker.add(bottomBaloon);
         initWidget(decorateDatePicker);
         panel.add(this.getMonthSelector());
         panel.add(this.getView());
+    }
+
+    public void toggle(String styleUp, String styleDown ) {
+        baloon.setStyleName("!!!");
+        baloon.removeStyleName(styleUp);
+        bottomBaloon.removeStyleName(styleDown);
+        baloon.setStyleName(styleUp);
+        bottomBaloon.setStyleName(styleDown);
+    }
+
+    public AbsolutePanel getBaloon() {
+        return baloon;
     }
 }
--- CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/form/widget/DateBoxDecorate.java
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/form/widget/DateBoxDecorate.java
@@ -1,9 +1,12 @@
 package ru.intertrust.cm.core.gui.impl.client.form.widget;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.datepicker.client.DateBox;
 
 import java.util.Date;
@@ -15,40 +18,73 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 public class DateBoxDecorate extends Composite {
-    //DateBox dateBox;
-    DateBox dateBox;
 
+    private DateBox dateBox;
+    private CMJDatePicker picker;
 
     public DateBoxDecorate(){
         AbsolutePanel root = new AbsolutePanel();
         root.setStyleName("wrap-date");
-        FocusPanel dateBtn = new FocusPanel();
+        final FocusPanel dateBtn = new FocusPanel();
         dateBtn.setStyleName("date-box-button");
 
         DateBox.Format format = new DateBox.DefaultFormat(DateTimeFormat.getFormat("dd/MM/yyyy"));
-        CMJDatePicker picker = new CMJDatePicker();
+        picker = new CMJDatePicker();
 
         //dateBox = new DateBox(picker, null, format);
         dateBox = new DateBox(picker, null, format);
+        TextBox textDateBox = dateBox.getTextBox();
         dateBox.getTextBox().addStyleName("date-text-box");
         root.add(dateBox);
         root.add(dateBtn);
+
+        textDateBox.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                String baloonUp;
+                String baloonDown;
+                dateBox.showDatePicker();
+
+                if(dateBtn.getAbsoluteTop() > dateBox.getDatePicker().getAbsoluteTop()){
+                    baloonUp =  "date-picker-baloon-up";
+                    baloonDown = "!!!";
+                }
+                else{
+                    baloonDown = "date-picker-baloon";
+                    baloonUp =  "!!!";
+                }
+
+                picker.toggle(baloonDown, baloonUp);
+
+            }
+        });
+
+        dateBtn.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                String baloonUp;
+                String baloonDown;
+                dateBox.showDatePicker();
+                if(dateBtn.getAbsoluteTop() > dateBox.getDatePicker().getAbsoluteTop()){
+                    baloonUp =  "date-picker-baloon-up";
+                    baloonDown = "!!!";
+                }
+                else{
+                    baloonDown = "date-picker-baloon";
+                    baloonUp =  "!!!";
+                }
+                picker.toggle(baloonDown, baloonUp);
+            }
+        });
+
         initWidget(root);
        }
 
     public void setValue(Date date) {
         dateBox.setValue(date, false);
-
-
-        //text.setText(DateBoxWidget.DATE_TIME_FORMAT.format(date));
     }
 
     public Date getValue() {
         return dateBox.getValue();
     }
-
-
-//    public TextBox getTextField() {
-//    //    return text;
-//    }
 }
