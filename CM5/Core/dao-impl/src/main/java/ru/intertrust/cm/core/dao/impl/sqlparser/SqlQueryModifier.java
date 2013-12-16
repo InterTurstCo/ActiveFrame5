37d6bdbc8e9260f2c31d446cf61fc41dde3a0975
atsvetkov <andrey.tsvetkov@augmentive.com>
2013-12-16 17:53:12 +0300

    Удаленние комита "temporary saving of data"
    This reverts commit 94836ccecb1e94296f99f357c245f1f01a70cc06.

--- CM5/Core/dao-impl/src/main/java/ru/intertrust/cm/core/dao/impl/sqlparser/SqlQueryModifier.java
+++ CM5/Core/dao-impl/src/main/java/ru/intertrust/cm/core/dao/impl/sqlparser/SqlQueryModifier.java
@@ -7,6 +7,8 @@ import static ru.intertrust.cm.core.dao.api.DomainObjectDao.REFERENCE_POSTFIX;
 import static ru.intertrust.cm.core.dao.impl.DataStructureNamingHelper.getReferenceTypeColumnName;
 import static ru.intertrust.cm.core.dao.impl.DataStructureNamingHelper.getServiceColumnName;
 import static ru.intertrust.cm.core.dao.impl.PostgreSqlQueryHelper.unwrap;
+import static ru.intertrust.cm.core.dao.impl.PostgreSqlQueryHelper.wrap;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
