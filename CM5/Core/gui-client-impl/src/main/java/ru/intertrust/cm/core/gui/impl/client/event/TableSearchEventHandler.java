3c46517ca2f9e01f7a327f0648e2888a86465063
alvov <alexey.v.lvov@gmail.com>
2013-12-24 17:22:27 +0200

    index on master: a0fb9f9 фильтровать по тексту не надо, если текста нет

---
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/event/TableSearchEventHandler.java
@@ -0,0 +1,11 @@
+package ru.intertrust.cm.core.gui.impl.client.event;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lvov
+ * Date: 23.12.13
+ * Time: 13:45
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TableSearchEventHandler {
+}
---
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/plugins/collection/CollectionSearchBox.java
@@ -0,0 +1,11 @@
+package ru.intertrust.cm.core.gui.impl.client.plugins.collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lvov
+ * Date: 17.12.13
+ * Time: 15:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class CollectionSearchBox {
+}
