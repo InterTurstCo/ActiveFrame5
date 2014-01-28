eeef91873715522b6874e620900882e38bc6d618
vmatsukevich <valery.matsukevich@augmentive.com>
2014-01-28 15:17:11 +0300

    CMFIVE-475:Возможность расширения конфигурации виджетов и плагинов, открываемых в навигационной панели

--- CM5/Core/model/src/main/java/ru/intertrust/cm/core/config/gui/form/widget/WidgetConfigurationConfig.java
+++ CM5/Core/model/src/main/java/ru/intertrust/cm/core/config/gui/form/widget/WidgetConfigurationConfig.java
@@ -18,31 +18,7 @@ import java.util.List;
 @Root(name = "widget-config")
 @Convert(WidgetConfigurationConfigConverter.class)
 public class WidgetConfigurationConfig implements Dto {
-    @ElementListUnion({
-            @ElementList(entry = "label", type = LabelConfig.class, inline = true, required = false),
-            @ElementList(entry = "rich-text-area", type = TextBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "integer-box", type = IntegerBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "text-box", type = TextBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "text-area", type = TextAreaConfig.class, inline = true, required = false),
-            @ElementList(entry = "decimal-box", type = DecimalBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "date-box", type = DateBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "combo-box", type = ComboBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "radio-button", type = RadioButtonConfig.class, inline = true, required = false),
-            @ElementList(entry = "linked-domain-object-hyperlink",
-                    type = LinkedDomainObjectHyperlinkConfig.class, inline = true, required = false),
-            @ElementList(entry = "linked-domain-objects-editable-table",
-                    type = LinkDomainObjectsEditableTableConfig.class, inline = true, required = false),
-            @ElementList(entry = "linked-domain-objects-table",
-                    type = LinkedDomainObjectsTableConfig.class, inline = true, required = false),
-            @ElementList(entry = "template-based-widget",
-                    type = TemplateBasedWidgetConfig.class, inline = true, required = false),
-            @ElementList(entry = "list-box", type = ListBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "suggest-box", type = SuggestBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "check-box", type = CheckBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "table-browser", type = TableBrowserConfig.class, inline = true, required = false),
-            @ElementList(entry = "attachment-box", type = AttachmentBoxConfig.class, inline = true, required = false),
-            @ElementList(entry = "hierarchy-browser", type = HierarchyBrowserConfig.class, inline = true, required = false)
-    })
+
     @ElementList(type=WidgetConfig.class, inline=true)
     private List<WidgetConfig> widgetConfigList = new ArrayList<WidgetConfig>();
 
