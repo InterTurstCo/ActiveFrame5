af3fe065ee1ce077e9d87856f1385b2251675d6e
vmatsukevich <valery.matsukevich@augmentive.com>
2014-01-30 18:10:43 +0300

    CMFIVE-475: Возможность расширения конфигурации виджетов и плагинов: xsd-валидация + пример использования

---
+++ CM5/test-module/src/main/java/ru/intertrust/cm/test/configuration/TestSpaceBarConfig.java
@@ -0,0 +1,55 @@
+package ru.intertrust.cm.test.configuration;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+import ru.intertrust.cm.core.business.api.dto.Dto;
+import ru.intertrust.cm.core.config.gui.form.widget.*;
+
+/**
+ * @author Yaroslav Bondacrhuk
+ *         Date: 13/9/13
+ *         Time: 12:05 PM
+ */
+@Root(name = "test-space-bar")
+public class TestSpaceBarConfig extends WidgetConfig implements Dto {
+
+    @Attribute
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getComponentName() {
+        return "test-space-bar";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        TestSpaceBarConfig that = (TestSpaceBarConfig) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
