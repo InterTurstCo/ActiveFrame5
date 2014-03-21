a152d9789ceefca20270ac2161cb3acb72b236b8
mlarin <larin@btlab.ru>
2014-03-21 17:13:36 +0400

    CMFIVE-615 Добавлены интерфейсы подсистемы профилей

--- CM5/Core/model/src/main/java/ru/intertrust/cm/core/business/api/dto/ProfileValue.java
+++ CM5/Core/model/src/main/java/ru/intertrust/cm/core/business/api/dto/ProfileValue.java
@@ -1,5 +1,20 @@
 package ru.intertrust.cm.core.business.api.dto;
 
+/**
+ * Интерфейс значения профиля
+ * @author larin
+ *
+ */
 public interface ProfileValue {
-
+    /**
+     * Получение флага только для чтения
+     * @return
+     */
+    boolean isReadOnly();
+    
+    /**
+     * Установка флага только для чтения
+     * @param readOnly
+     */
+    void setReadOnly(boolean readOnly);
 }
