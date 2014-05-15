a6c82cfe4a4e0aaabff3c4fa0c7cc005535b92a2
atsvetkov <andrey.tsvetkov@augmentive.com>
2014-05-15 19:56:16 +0300

    CMFIVE-859: Добавлен мэйвен профайл для запуска интеграционных тестов в embeded режиме (необходим для Jenkins).

--- CM5/ItTests/src/test/java/ru/intertrust/cm/core/service/it/ConfigurationControlIT.java
+++ CM5/ItTests/src/test/java/ru/intertrust/cm/core/service/it/ConfigurationControlIT.java
@@ -1,42 +1,51 @@
 package ru.intertrust.cm.core.service.it;
 
-import org.jboss.arquillian.junit.Arquillian;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import ru.intertrust.cm.core.business.api.ConfigurationControlService;
-import ru.intertrust.cm.core.business.api.ConfigurationService;
-import ru.intertrust.cm.core.config.DomainObjectConfig;
-import ru.intertrust.cm.core.config.DomainObjectTypeConfig;
-import ru.intertrust.cm.core.config.FieldConfig;
-import ru.intertrust.cm.core.config.GlobalSettingsConfig;
-import ru.intertrust.cm.core.config.base.CollectionConfig;
-import ru.intertrust.cm.core.config.base.Configuration;
-import ru.intertrust.cm.core.config.gui.collection.view.CollectionColumnConfig;
-import ru.intertrust.cm.core.config.gui.collection.view.CollectionViewConfig;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-import javax.ejb.EJB;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collection;
 
-import static org.junit.Assert.*;
+import javax.ejb.EJB;
+import javax.security.auth.login.LoginException;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+
+import ru.intertrust.cm.core.business.api.ConfigurationControlService;
+import ru.intertrust.cm.core.business.api.ConfigurationService;
+import ru.intertrust.cm.core.config.DomainObjectTypeConfig;
+import ru.intertrust.cm.core.config.base.CollectionConfig;
+import ru.intertrust.cm.webcontext.ApplicationContextProvider;
 
 /**
  * Иитеграционный тест для {@link ru.intertrust.cm.core.business.api.ConfigurationService} и {@link ru.intertrust.cm.core.business.api.ConfigurationControlService}
  * Created by vmatsukevich on 2/4/14.
  *
  */
-@RunWith(Arquillian.class)
+//@RunWith(Arquillian.class)
 public class ConfigurationControlIT extends IntegrationTestBase {
 
-    @EJB
     private ConfigurationControlService configurationControlService;
 
-    @EJB
+//    @EJB
     private ConfigurationService.Remote configurationService;
 
-    @Test
+//    @Before
+    public void init() throws IOException, LoginException {
+        initializeSpringBeans();        
+    }
+    private void initializeSpringBeans() {
+        ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
+        configurationControlService = applicationContext.getBean(ConfigurationControlService.class);
+    }
+    
+//    @Test
     public void testConfigurationLoaded() throws IOException {
         byte[] fileBytes = Files.readAllBytes(Paths.get("test-data/configuration-test.xml"));
         String configuration = new String(fileBytes);
