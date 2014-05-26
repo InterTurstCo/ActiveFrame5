02e0d506425920fbc502383c1d64c794d48d493d
ybondarchuk <yaroslav.i.bondarchuk@gmail.com>
2014-05-26 20:05:43 +0300

    CMFIVE-618:  Иерархический браузер недобавленый файл

---
+++ CM5/Core/model/src/main/java/ru/intertrust/cm/core/config/gui/form/widget/RootNodeLinkConfig.java
@@ -0,0 +1,43 @@
+package ru.intertrust.cm.core.config.gui.form.widget;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Root;
+import ru.intertrust.cm.core.business.api.dto.Dto;
+
+/**
+ * @author Yaroslav Bondarchuk
+ *         Date: 10.12.13
+ *         Time: 11:40
+ */
+@Root(name = "root-node-link")
+public class RootNodeLinkConfig implements Dto{
+    @Attribute(name = "title", required = true)
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        RootNodeLinkConfig that = (RootNodeLinkConfig) o;
+
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return title != null ? title.hashCode() : 0;
+    }
+}
