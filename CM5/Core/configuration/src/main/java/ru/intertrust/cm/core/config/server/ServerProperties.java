28ac587b428b34edf24c43b01503d6826fb39f32
rmitenkov <roman.mitenkov@augmentive.com>
2014-06-24 16:55:29 +0300

    CMFIVE-833: для работы нескольких приложений на одном сервере свойства сервера могут зачитываться из файла <application-name>-server.properties

--- CM5/Core/configuration/src/main/java/ru/intertrust/cm/core/config/server/ServerProperties.java
+++ CM5/Core/configuration/src/main/java/ru/intertrust/cm/core/config/server/ServerProperties.java
@@ -1,12 +1,20 @@
 package ru.intertrust.cm.core.config.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.File;
 
 @Configuration
-@PropertySource({ "classpath:/server-default.properties","file:${server.properties.location}/server.properties" })
 public class ServerProperties {
 
     @Bean
@@ -14,6 +22,32 @@ public class ServerProperties {
         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
         configurer.setNullValue("");
         //configurer.setIgnoreUnresolvablePlaceholders(true);
+
+        String serverPropertiesLocation = System.getProperty("server.properties.location");
+
+        String appName = "";
+        try {
+            appName = (String) new InitialContext().lookup("java:app/AppName");
+        } catch (NamingException e) {
+        }
+
+        FileSystemResource serverPropertiesResource;
+
+        File applicationSpecificServerProp = new File(serverPropertiesLocation, appName + "-server.properties");
+        if (applicationSpecificServerProp.exists()){
+            serverPropertiesResource = new FileSystemResource(applicationSpecificServerProp);
+        } else {
+            serverPropertiesResource = new FileSystemResource(serverPropertiesLocation + "/server.properties");
+        }
+
+
+        Resource[] resources = new Resource[ ]
+                {
+                        new ClassPathResource( "server-default.properties" ),
+                        serverPropertiesResource
+                };
+
+        configurer.setLocations(resources);
         return configurer;
     }
 }
