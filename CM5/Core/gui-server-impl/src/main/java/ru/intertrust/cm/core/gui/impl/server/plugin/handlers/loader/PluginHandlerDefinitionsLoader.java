3660f486db92f20a2fd456f94da992ffcaf6d554
dmitavskiy <dmitavskiy@inttrust.ru>
2014-06-27 19:58:58 +0300

    CMFIVE-1200: вновь исправлен ClassCast - под Linux

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/plugin/handlers/loader/PluginHandlerDefinitionsLoader.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/plugin/handlers/loader/PluginHandlerDefinitionsLoader.java
@@ -7,14 +7,17 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.ScannedGenericBeanDefinition;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.core.type.filter.AssignableTypeFilter;
 import org.springframework.core.type.filter.RegexPatternTypeFilter;
+import org.springframework.core.type.filter.TypeFilter;
 import ru.intertrust.cm.core.gui.api.server.ComponentHandler;
 import ru.intertrust.cm.core.gui.model.ComponentName;
 
-import java.util.Map;
-import java.util.Set;
+import java.io.IOException;
+import java.util.*;
 import java.util.regex.Pattern;
 
 /**
@@ -29,13 +32,12 @@ public class PluginHandlerDefinitionsLoader implements BeanDefinitionRegistryPos
 
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-        ClassPathScanningCandidateComponentProvider componentProvider = new ClassPathScanningCandidateComponentProvider(false);
-        componentProvider.addIncludeFilter(new AnnotationTypeFilter(ComponentName.class));
-        componentProvider.addIncludeFilter(new AssignableTypeFilter(ComponentHandler.class));
-        componentProvider.addExcludeFilter(new RegexPatternTypeFilter(Pattern.compile("ru.intertrust.cm.core.gui.api.codegen.ComponentRegistryGenerator")));
+        ScanningProvider componentProvider = new ScanningProvider();
         Set<BeanDefinition> pluginHandlerDefinitions = componentProvider.findCandidateComponents(basePackage);
 
+        System.out.println("================================================ REGISTERED COMPONENTS =========================================================");
         for (BeanDefinition pluginHandlerDefinition : pluginHandlerDefinitions) {
+            System.out.println(pluginHandlerDefinition.getBeanClassName());
             registerBeanDefinitionInRegistry((ScannedGenericBeanDefinition) pluginHandlerDefinition, registry);
         }
     }
@@ -58,5 +60,42 @@ public class PluginHandlerDefinitionsLoader implements BeanDefinitionRegistryPos
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         // no post processing
+    }
+
+    /**
+     * Custom scanner - implementing AND-logic of filtering instead of Spring's OR-logic
+     */
+    private static class ScanningProvider extends ClassPathScanningCandidateComponentProvider {
+        private MetadataReaderFactory metadataReaderFactory;
+        private List<TypeFilter> includeFilters = new ArrayList<>(2);
+        private List<TypeFilter> excludeFilters = new ArrayList<>(2);
+
+        public ScanningProvider() {
+            super(false);
+            metadataReaderFactory = getMetadataReaderFactory();
+            this.includeFilters.add(new AnnotationTypeFilter(ComponentName.class));
+            this.includeFilters.add(new AssignableTypeFilter(ComponentHandler.class));
+            this.excludeFilters.add(new RegexPatternTypeFilter(Pattern.compile("ru.intertrust.cm.core.gui.api.codegen.ComponentRegistryGenerator")));
+        }
+
+        /**
+         * Determine whether the given class does not match any exclude filter
+         * and does match at least one include filter.
+         * @param metadataReader the ASM ClassReader for the class
+         * @return whether the class qualifies as a candidate component
+         */
+        protected boolean isCandidateComponent(MetadataReader metadataReader) throws IOException {
+            for (TypeFilter tf : this.excludeFilters) {
+                if (tf.match(metadataReader, this.metadataReaderFactory)) {
+                    return false;
+                }
+            }
+            for (TypeFilter tf : this.includeFilters) {
+                if (!tf.match(metadataReader, this.metadataReaderFactory)) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
