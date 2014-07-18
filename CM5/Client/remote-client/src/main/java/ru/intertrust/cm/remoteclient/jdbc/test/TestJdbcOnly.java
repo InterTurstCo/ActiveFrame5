211fcbb8424a6be25ccd7d9c8941f1080efc4104
mlarin <larin@btlab.ru>
2014-07-18 16:43:23 +0400

    CMFIVE-1312 Добавлен тест JDBC с параметром типа Boolean

--- CM5/Client/remote-client/src/main/java/ru/intertrust/cm/remoteclient/jdbc/test/TestJdbcOnly.java
+++ CM5/Client/remote-client/src/main/java/ru/intertrust/cm/remoteclient/jdbc/test/TestJdbcOnly.java
@@ -9,31 +9,41 @@ import java.sql.SQLException;
 import ru.intertrust.cm.core.jdbc.JdbcDriver;
 
 public class TestJdbcOnly {
-	public static void main(String[] args) {
-		try {
-			TestJdbcOnly test = new TestJdbcOnly();
-			test.execute(args);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            TestJdbcOnly test = new TestJdbcOnly();
+            test.execute(args);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	private void execute(String[] args) throws Exception {
-		Class.forName(JdbcDriver.class.getName());
+    private void execute(String[] args) throws Exception {
+        Class.forName(JdbcDriver.class.getName());
 
-		Connection connection = DriverManager.getConnection(
-				"jdbc:sochi:remoting://localhost:4447/cm-sochi/web-app", "admin", "admin");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:sochi:remoting://localhost:4447/cm-sochi/web-app", "admin", "admin");
 
-		String query = "select t.id, t.login, t.created_date, t.status ";
-		query += "from person t ";
-		executeQuery(connection, query);
+        String query = "select t.id, t.login, t.created_date, t.status ";
+        query += "from person t ";
+        executeQuery(connection, query);
 
-		executeQuery(connection, "select dateon, DateOff, DateAll from tst_employee");
-		connection.close();
-	}
-	
-	private void executeQuery(Connection connection, String sql) throws Exception{
+        executeQuery(connection, "select dateon, DateOff, DateAll from tst_employee");
+
+        executeQuery(connection, "select id from tst_employee where (active=? and 1<>?)", true, true);
+
+        connection.close();
+    }
+
+    private void executeQuery(Connection connection, String sql, Object... params) throws Exception {
         PreparedStatement prepareStatement = connection.prepareStatement(sql);
+
+        for (int i = 0; i < params.length; i++) {
+            Object param = params[i];
+            if (param instanceof Boolean) {
+                prepareStatement.setBoolean(i + 1, (Boolean) param);
+            }
+        }
 
         ResultSet resultset = prepareStatement.executeQuery();
 
@@ -41,22 +51,22 @@ public class TestJdbcOnly {
 
         resultset.close();
         prepareStatement.close();
-	}
+    }
 
-	private void printResultSet(ResultSet resultset) throws SQLException {
-		System.out.print("№\t");
-		for (int i = 1; i <= resultset.getMetaData().getColumnCount(); i++) {
-			System.out.print(resultset.getMetaData().getColumnName(i) + "\t");
-		}
-		System.out.print("\n");
-		int rowCount = 0;
-		while (resultset.next()) {
-			System.out.print(rowCount + "\t");
-			for (int i = 1; i <= resultset.getMetaData().getColumnCount(); i++) {
-				System.out.print(resultset.getObject(i) + "\t");
-			}
-			System.out.print("\n");
-			rowCount++;
-		}
-	}
+    private void printResultSet(ResultSet resultset) throws SQLException {
+        System.out.print("№\t");
+        for (int i = 1; i <= resultset.getMetaData().getColumnCount(); i++) {
+            System.out.print(resultset.getMetaData().getColumnName(i) + "\t");
+        }
+        System.out.print("\n");
+        int rowCount = 0;
+        while (resultset.next()) {
+            System.out.print(rowCount + "\t");
+            for (int i = 1; i <= resultset.getMetaData().getColumnCount(); i++) {
+                System.out.print(resultset.getObject(i) + "\t");
+            }
+            System.out.print("\n");
+            rowCount++;
+        }
+    }
 }
