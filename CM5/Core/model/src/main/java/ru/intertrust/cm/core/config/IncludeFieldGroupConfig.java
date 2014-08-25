359e3c3d0356f38078ad3e8cd2f968d8ed1cbbde
atsvetkov <andrey.tsvetkov@augmentive.com>
2014-08-25 19:09:27 +0300

    CMFIVE-97: Конфигурация блоков полей. Фикс сериализации.

--- CM5/Core/model/src/main/java/ru/intertrust/cm/core/config/IncludeFieldGroupConfig.java
+++ CM5/Core/model/src/main/java/ru/intertrust/cm/core/config/IncludeFieldGroupConfig.java
@@ -1,11 +1,13 @@
 package ru.intertrust.cm.core.config;
 
+import ru.intertrust.cm.core.business.api.dto.Dto;
+
 
 
 /**
  * Конфигурация подключения группы полей к конфигурации доменного объекта.
  * @author atsvetkov
  */
-public class IncludeFieldGroupConfig extends NamedTypeConfig {
+public class IncludeFieldGroupConfig extends NamedTypeConfig implements Dto {
     
 }
