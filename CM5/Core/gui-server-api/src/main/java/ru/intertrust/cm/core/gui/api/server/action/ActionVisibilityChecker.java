b49168c987da76a246eb915b374ba83732dad617
sokolot <sergey.n.okolot@gmail.com>
2014-09-24 16:34:40 +0300

    CMFIVE-1342 (Действия - условия отображ) updated

--- CM5/Core/gui-server-api/src/main/java/ru/intertrust/cm/core/gui/api/server/action/ActionVisibilityChecker.java
+++ CM5/Core/gui-server-api/src/main/java/ru/intertrust/cm/core/gui/api/server/action/ActionVisibilityChecker.java
@@ -1,10 +1,12 @@
 package ru.intertrust.cm.core.gui.api.server.action;
 
+import ru.intertrust.cm.core.gui.api.server.ComponentHandler;
+
 /**
  * @author Sergey.Okolot
  *         Created on 01.09.2014 17:03.
  */
-public interface ActionVisibilityChecker {
+public interface ActionVisibilityChecker extends ComponentHandler {
 
     boolean isVisible(ActionVisibilityContext context);
 }
