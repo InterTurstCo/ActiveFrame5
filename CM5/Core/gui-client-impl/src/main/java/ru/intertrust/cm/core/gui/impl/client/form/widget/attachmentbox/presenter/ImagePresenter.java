913fba1545aa766ce13ea10f9ccd494ce82ca9fb
lpuhova <lesia.puhova@gmail.com>
2014-10-24 20:18:12 +0300

    CMFIVE - 1898 -  AttachmentBoxWidget - исправлено масштабирование картинки

--- CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/form/widget/attachmentbox/presenter/ImagePresenter.java
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/form/widget/attachmentbox/presenter/ImagePresenter.java
@@ -5,6 +5,7 @@ import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Panel;
 import ru.intertrust.cm.core.config.gui.form.widget.PreviewConfig;
+import ru.intertrust.cm.core.gui.impl.client.form.widget.attachmentbox.ScalePreviewHandler;
 import ru.intertrust.cm.core.gui.model.form.widget.AttachmentItem;
 
 /**
@@ -13,9 +14,6 @@ import ru.intertrust.cm.core.gui.model.form.widget.AttachmentItem;
  *         Time: 13:59
  */
 class ImagePresenter implements AttachmentElementPresenter {
-
-    private static final int DEFAULT_WIDTH = 100;
-    private static final int DEFAULT_HEIGHT = 100;
 
     private PreviewConfig previewConfig;
     private ClickHandler clickHandler;
@@ -29,93 +27,36 @@ class ImagePresenter implements AttachmentElementPresenter {
 
     @Override
     public Panel presentElement() {
-        Panel element = new AbsolutePanel();
-
+        final Panel element = new AbsolutePanel();
         element.addStyleName("facebook-element");
         element.addStyleName("image-preview");
-//        Panel panel = new SimplePanel();
-//        panel.addStyleName("image-preview");
-        Image image = new ScalableImage(previewConfig);
-        StringBuilder url = new StringBuilder(com.google.gwt.core.client.GWT.getHostPageBaseURL())
-                .append("image-preview?");
-        if (item.getId() != null) {
-                url.append("id=").append(item.getId().toStringRepresentation());
+
+        if (previewConfig.getWidth() != null) {
+            element.setWidth(previewConfig.getWidth());
         }
-        if (item.getTemporaryName() != null) {
-            url.append("tempName=").append(item.getTemporaryName());
+        if (previewConfig.getHeight() != null) {
+            element.setHeight(previewConfig.getHeight());
         }
-        image.setUrl(url.toString());
+
+        final Image image = new Image(createPreviewUrl(item));
+        image.addLoadHandler(new ScalePreviewHandler(previewConfig, image));
         if (clickHandler != null) {
             image.addClickHandler(clickHandler);
         }
-
-//        panel.add(image);
-//        panel.setWidth(previewConfig.getWidth() != null ? previewConfig.getWidth() : DEFAULT_WIDTH + "px");
-//        panel.setHeight(previewConfig.getHeight() != null ? previewConfig.getHeight() : DEFAULT_HEIGHT + "px");
-//        element.add(panel);
-//        element.setWidth(previewConfig.getWidth() != null ? previewConfig.getWidth() : DEFAULT_WIDTH + "px");
-//        element.setHeight(previewConfig.getHeight() != null ? previewConfig.getHeight() : DEFAULT_HEIGHT + "px");
         element.add(image);
         return element;
     }
 
-
-
-    private static class ScalableImage extends Image {
-
-        private PreviewConfig previewConfig;
-
-        ScalableImage(PreviewConfig previewConfig) {
-            this.previewConfig = previewConfig;
+    private static String createPreviewUrl(AttachmentItem item) {
+        StringBuilder url = new StringBuilder(com.google.gwt.core.client.GWT.getHostPageBaseURL())
+                .append("image-preview?");
+        if (item.getId() != null) {
+            url.append("id=").append(item.getId().toStringRepresentation());
         }
-
-        protected void onAttach(){
-          //  Window.alert(getWidth() + " : " + getHeight());
-          //  setupSizes(this, previewConfig);
-            super.onAttach();
+        if (item.getTemporaryName() != null) {
+            url.append("tempName=").append(item.getTemporaryName());
         }
-
-        private static void setupSizes(Image image, PreviewConfig config) {
-            if (!config.isPreserveProportion()) {
-                image.setWidth(config.getWidth() != null ?  config.getWidth() : DEFAULT_WIDTH + "px");
-                image.setHeight(config.getHeight() != null ? config.getHeight() : DEFAULT_HEIGHT + "px");
-            } else {
-                int origWidth = image.getWidth() != 0 ? image.getWidth() : DEFAULT_WIDTH;
-                int origHeight = image.getHeight() != 0 ? image.getHeight() : DEFAULT_HEIGHT;
-
-                int width;
-                int height;
-                if (config.getWidth() == null && config.getHeight() == null) {
-                    width = origWidth;
-                    height = origHeight;
-                } else if (config.getWidth() != null && config.getHeight() == null) {
-                    image.setWidth(config.getWidth());
-                    width = image.getWidth();
-                    height = origHeight * width / origWidth;
-                } else if (config.getHeight() != null && config.getWidth() == null) {
-                    image.setHeight(config.getHeight());
-                    height = image.getHeight();
-                    width = origWidth * height / origHeight;
-                } else {
-                    image.setWidth(config.getWidth());
-                    image.setHeight(config.getHeight());
-                    int maxWidth = image.getWidth();
-                    int maxHeight = image.getHeight();
-
-                    width = maxWidth;
-                    height = origHeight * width;
-                    height = height / origWidth;
-
-                    if (height > maxHeight) {
-                        origWidth = width;
-                        origHeight = height;
-
-                        height = maxHeight;
-                        width = origWidth * height / origHeight;
-                    }
-                }
-                image.setPixelSize(width, height);
-            }
-        }
+        return url.toString();
     }
+
 }
