e3b365a5623a43a0724162cb9351c6cfe119ada0
dmitavskiy <dmitavskiy@inttrust.ru>
2015-03-27 18:29:04 +0300

    CMFIVE-3602: недостающие методы

--- CM5/Core/gui-server-api/src/main/java/ru/intertrust/cm/core/gui/api/server/widget/ValueEditingWidgetHandler.java
+++ CM5/Core/gui-server-api/src/main/java/ru/intertrust/cm/core/gui/api/server/widget/ValueEditingWidgetHandler.java
@@ -1,17 +1,9 @@
 package ru.intertrust.cm.core.gui.api.server.widget;
 
-import ru.intertrust.cm.core.config.FieldConfig;
-import ru.intertrust.cm.core.gui.model.form.FieldPath;
-
 /**
  * @author Denis Mitavskiy
  *         Date: 14.10.13
  *         Time: 14:58
  */
 public abstract class ValueEditingWidgetHandler extends WidgetHandler {
-    protected FieldConfig getFieldConfig(WidgetContext context) {
-        final FieldPath fieldPath = context.getFirstFieldPath();
-        String parentType = context.getFormObjects().getParentNode(fieldPath).getType();
-        return configurationService.getFieldConfig(parentType, fieldPath.getFieldName());
-    }
 }
