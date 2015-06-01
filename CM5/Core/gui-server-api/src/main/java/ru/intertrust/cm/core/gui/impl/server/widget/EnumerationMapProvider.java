f372161f4e46ab7e75c5e12b11f5b9735c62ed8d
Andrey Pirozhkov <apirozhkov@inttrust.ru>
2015-06-01 21:54:49 +0300

    CMFIVE-3933: Добавлена страница управления периодическими задачами (в тестовое приложение), а также код поддержки контролов этой страницы (в ядро)

--- CM5/Core/gui-server-api/src/main/java/ru/intertrust/cm/core/gui/impl/server/widget/EnumerationMapProvider.java
+++ CM5/Core/gui-server-api/src/main/java/ru/intertrust/cm/core/gui/impl/server/widget/EnumerationMapProvider.java
@@ -1,6 +1,7 @@
 package ru.intertrust.cm.core.gui.impl.server.widget;
 
 import ru.intertrust.cm.core.business.api.dto.Value;
+import ru.intertrust.cm.core.gui.api.server.ComponentHandler;
 import ru.intertrust.cm.core.gui.api.server.widget.WidgetContext;
 
 import java.util.Map;
@@ -10,6 +11,6 @@ import java.util.Map;
  *         Date: 07.10.14
  *         Time: 12:20
  */
-public interface EnumerationMapProvider {
+public interface EnumerationMapProvider extends ComponentHandler {
     Map<String, Value> getMap(WidgetContext widgetContext);
 }
