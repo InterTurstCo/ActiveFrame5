60e5b48d7b1ee926c89accd3117113ae8217e74e
ybondarchuk <yaroslav.i.bondarchuk@gmail.com>
2015-07-27 23:54:43 +0300

    CMFIVE-4102: Фикс "Не работает FormDefaultValueSetter для date-box"

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/widget/DateBoxHandler.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/widget/DateBoxHandler.java
@@ -31,8 +31,6 @@ public class DateBoxHandler extends ValueEditingWidgetHandler {
         final DateBoxConfig config = widgetContext.getWidgetConfig();
         final DateBoxState state = new DateBoxState();
         state.setPattern(config.getPattern());
-
-
         FieldType fieldType = FormConfig.TYPE_REPORT.equals(widgetContext.getFormType()) ?
                 getFieldTypeForReportForm(config) : getFieldType(widgetContext);
 
@@ -41,7 +39,7 @@ public class DateBoxHandler extends ValueEditingWidgetHandler {
         state.setDisplayTimeZoneChoice(displayTimeZoneChoice);
         final DateValueConverter converter = getConverter(fieldType);
         final DateFormat dateFormat = ThreadSafeDateFormat.getDateFormat(new Pair<String, Locale>(ModelUtil.DTO_PATTERN, null), null);
-        Value value = FormConfig.TYPE_REPORT.equals(widgetContext.getFormType()) ? null : widgetContext.getValue();
+        Value value = widgetContext.getValue();
         final DateTimeContext context = converter.valueToContext(value, config.getTimeZoneId(), dateFormat);
         context.setTimeZoneId(config.getTimeZoneId());
         state.setDateTimeContext(context);
@@ -108,145 +106,4 @@ public class DateBoxHandler extends ValueEditingWidgetHandler {
         }
     }
 
-   /* private interface DateValueConverter<T extends Value> {
-
-        DateTimeContext valueToContext(T value, DateBoxConfig config);
-
-        T contextToValue(DateTimeContext context);
-    }
-
-    private static class TimelessDateValueConverter implements DateValueConverter<TimelessDateValue> {
-        @Override
-        public DateTimeContext valueToContext(TimelessDateValue value, DateBoxConfig config) {
-            final DateTimeContext result = new DateTimeContext();
-            result.setOrdinalFieldType(FieldType.TIMELESSDATE.ordinal());
-            if (value != null && value.get() != null) {
-                final Calendar calendar =
-                        GuiServerHelper.timelessDateToCalendar(value.get(), GuiServerHelper.GMT_TIME_ZONE);
-                final DateFormat dateFormat = new SimpleDateFormat(ModelUtil.DTO_PATTERN);
-                final String timeZoneId = GuiContext.get().getUserInfo().getTimeZoneId();
-                dateFormat.setTimeZone(TimeZone.getTimeZone(timeZoneId));
-                final String dateTime = dateFormat.format(calendar.getTime());
-                result.setDateTime(dateTime);
-            }
-            return result;
-        }
-
-        @Override
-        public TimelessDateValue contextToValue(final DateTimeContext context) {
-            if (context.getDateTime() != null) {
-                final TimelessDate result = new TimelessDate();
-                final String timeZoneId = GuiContext.get().getUserInfo().getTimeZoneId();
-                final DateFormat dateFormat = new SimpleDateFormat(ModelUtil.DTO_PATTERN);
-                dateFormat.setTimeZone(TimeZone.getTimeZone(timeZoneId));
-                try {
-                    final Date date = dateFormat.parse(context.getDateTime());
-                    final Calendar calendar = Calendar.getInstance(GuiServerHelper.GMT_TIME_ZONE);
-                    calendar.setTime(date);
-                    result.setYear(calendar.get(Calendar.YEAR));
-                    result.setMonth(calendar.get(Calendar.MONTH));
-                    result.setDayOfMonth(calendar.get(Calendar.DAY_OF_MONTH));
-                    return new TimelessDateValue(result);
-                } catch (ParseException ignored) {
-                    ignored.printStackTrace();  // for developers only
-                }
-            }
-            return new TimelessDateValue();
-        }
-    }
-
-    private static class DateTimeValueConverter implements DateValueConverter<DateTimeValue> {
-        @Override
-        public DateTimeContext valueToContext(DateTimeValue value, DateBoxConfig config) {
-            final DateTimeContext result = new DateTimeContext();
-            result.setOrdinalFieldType(FieldType.DATETIME.ordinal());
-            if (value != null && value.get() != null) {
-                final String timeZoneId = getTimeZoneId(result, config.getTimeZoneId());
-                final DateFormat dateFormat = new SimpleDateFormat(ModelUtil.DTO_PATTERN);
-                dateFormat.setTimeZone(TimeZone.getTimeZone(timeZoneId));
-                result.setDateTime(dateFormat.format(value.get()));
-            }
-
-            return result;
-        }
-
-        @Override
-        public DateTimeValue contextToValue(final DateTimeContext context) {
-            final DateTimeValue result = new DateTimeValue();
-            if (context.getDateTime() != null) {
-                final String timeZoneId = getTimeZoneId(context, context.getTimeZoneId());
-                final DateFormat dateFormat = new SimpleDateFormat(ModelUtil.DTO_PATTERN);
-                dateFormat.setTimeZone(TimeZone.getTimeZone(timeZoneId));
-                try {
-                    final Date date = dateFormat.parse(context.getDateTime());
-                    result.setValue(date);
-                } catch (ParseException ignored) {
-                    ignored.printStackTrace(); // for developers only
-                }
-            }
-            return result;
-        }
-
-        protected String getTimeZoneId(final DateTimeContext context, final String timeZoneId) {
-            switch (timeZoneId) {
-                case ModelUtil.DEFAULT_TIME_ZONE_ID:
-                case ModelUtil.LOCAL_TIME_ZONE_ID:
-                case ModelUtil.ORIGINAL_TIME_ZONE_ID:
-                    return GuiContext.get().getUserInfo().getTimeZoneId();
-            }
-            return context.getTimeZoneId();
-        }
-    }
-
-    private static class DateTimeWithTimezoneValueConverter implements DateValueConverter<DateTimeWithTimeZoneValue> {
-        @Override
-        public DateTimeContext valueToContext(DateTimeWithTimeZoneValue value, DateBoxConfig config) {
-            final DateTimeContext result = new DateTimeContext();
-            result.setOrdinalFieldType(FieldType.DATETIMEWITHTIMEZONE.ordinal());
-            if (value != null && value.get() != null) {
-                final Calendar calendar = GuiServerHelper.dateTimeWithTimezoneToCalendar(value.get());
-                final DateFormat dateFormat = new SimpleDateFormat(ModelUtil.DTO_PATTERN);
-                final String userTimeZoneId = GuiContext.get().getUserInfo().getTimeZoneId();
-                dateFormat.setTimeZone(TimeZone.getTimeZone(userTimeZoneId));
-                result.setDateTime(dateFormat.format(calendar.getTime()));
-                result.setTimeZoneId(value.get().getTimeZoneContext().getTimeZoneId());
-            }
-            return result;
-        }
-
-        @Override
-        public DateTimeWithTimeZoneValue contextToValue(final DateTimeContext context) {
-            if (context.getDateTime() != null) {
-                final String userTimeZoneId = GuiContext.get().getUserInfo().getTimeZoneId();
-                final DateFormat dateFormat = new SimpleDateFormat(ModelUtil.DTO_PATTERN);
-                dateFormat.setTimeZone(TimeZone.getTimeZone(userTimeZoneId));
-                try {
-                    final Date date = dateFormat.parse(context.getDateTime());
-                    final String rawTimeZoneId = context.getTimeZoneId();
-                    final String timeZoneId = DateUtil.getTimeZoneId(rawTimeZoneId);
-                    final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(timeZoneId));
-                    calendar.setTime(date);
-                    final DateTimeWithTimeZone dateTimeWithTimeZone = new DateTimeWithTimeZone();
-                    dateTimeWithTimeZone.setYear(calendar.get(Calendar.YEAR));
-                    dateTimeWithTimeZone.setMonth(calendar.get(Calendar.MONTH));
-                    dateTimeWithTimeZone.setDayOfMonth(calendar.get(Calendar.DAY_OF_MONTH));
-                    dateTimeWithTimeZone.setHours(calendar.get(Calendar.HOUR_OF_DAY));
-                    dateTimeWithTimeZone.setMinutes(calendar.get(Calendar.MINUTE));
-                    dateTimeWithTimeZone.setSeconds(calendar.get(Calendar.SECOND));
-                    dateTimeWithTimeZone.setMilliseconds(calendar.get(Calendar.MILLISECOND));
-                    if (timeZoneId.startsWith("GMT")) {
-                        TimeZone timeZone = TimeZone.getTimeZone(timeZoneId);
-                        dateTimeWithTimeZone.setTimeZoneContext(new UTCOffsetTimeZoneContext(timeZone.getRawOffset()));
-                    } else {
-                        dateTimeWithTimeZone.setTimeZoneContext(new OlsonTimeZoneContext(timeZoneId));
-                    }
-                    return new DateTimeWithTimeZoneValue(dateTimeWithTimeZone);
-                } catch (ParseException ignored) {
-                    ignored.printStackTrace(); // for developers only
-                }
-            }
-            return new DateTimeWithTimeZoneValue();
-        }
-
-    }*/
 }
