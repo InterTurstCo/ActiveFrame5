74c4679df36886892d3e60a8c801a176343bbfd1
mlarin <larin@btlab.ru>
2015-09-08 14:04:53 +0300

    CMFIVE-4281 Тест для исследования производительности команд insert и update

--- CM5/Client/remote-client/src/main/java/ru/intertrust/cm/remoteclient/crud/test/TestLageDomainObject.java
+++ CM5/Client/remote-client/src/main/java/ru/intertrust/cm/remoteclient/crud/test/TestLageDomainObject.java
@@ -9,13 +9,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import javax.naming.OperationNotSupportedException;
 import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import ru.intertrust.cm.core.dao.impl.BatchPreparedStatementSetter;
 
 /**
  * Тестирование времени выполнения JDBC запроса при вставке или обновления
@@ -27,12 +31,18 @@ public class TestLageDomainObject {
     public static final int MAX_FIELDS_COUNT = 200;
     public static final int FIELDS_STEP = 10;
     public static final int QUERY_FIELDS_STEP = 10;
-    public static final int ITERATIONS_COUNT = 1;
+    public static final int ITERATIONS_COUNT = 5;
 
     private ApplicationContext applicationContext;
     private NamedParameterJdbcTemplate jdbcTemplate;
+    private JdbcOperations jdbcOperations;
     private DataSource dataSource;
-    private boolean useSpring;
+
+    public enum TestType{
+        NamedParameterJdbctemplate,
+        PrepareStatment,
+        JdbcOperation
+    }
 
     private List<Long> ids = new ArrayList<Long>();
     private Random rnd = new Random();
@@ -46,46 +56,43 @@ public class TestLageDomainObject {
         }
     }
 
-    private void execute(String[] args) throws SQLException {
+    private void execute(String[] args) throws SQLException, OperationNotSupportedException {
         //Создание spring контекста
         applicationContext = new ClassPathXmlApplicationContext("classpath:/TestLageDomainObject.xml");
         jdbcTemplate = applicationContext.getBean(NamedParameterJdbcTemplate.class);
         dataSource = applicationContext.getBean(DataSource.class);
+        jdbcOperations = applicationContext.getBean(JdbcOperations.class);
 
         dropTable();
 
         // Создание таблицы c разным кол во атрибутов
-        List<List<Long>> resultsInsertSpring0 = new ArrayList<List<Long>>();
-        List<List<Long>> resultsUpdateSpring0 = new ArrayList<List<Long>>();
-        List<List<Long>> resultsInsertSpring1 = new ArrayList<List<Long>>();
-        List<List<Long>> resultsUpdateSpring1 = new ArrayList<List<Long>>();
-
-        List<List<Long>> resultsInsertJdbc0 = new ArrayList<List<Long>>();
-        List<List<Long>> resultsUpdateJdbc0 = new ArrayList<List<Long>>();
+        List<List<Long>> resultsInsertNameParam = new ArrayList<List<Long>>();
+        List<List<Long>> resultsUpdateNameParam = new ArrayList<List<Long>>();
+        List<List<Long>> resultsInsertJdbcOperation = new ArrayList<List<Long>>();
+        List<List<Long>> resultsUpdateJdbcOperation = new ArrayList<List<Long>>();
+        List<List<Long>> resultsInsertPrepareStatment = new ArrayList<List<Long>>();
+        List<List<Long>> resultsUpdatePrepareStatment = new ArrayList<List<Long>>();
 
         for (int i = 10; i <= MAX_FIELDS_COUNT; i = i + FIELDS_STEP) {
             System.out.println("Table with " + i + " fields");
             createTable(i);
 
-            useSpring = true;
-            resultsInsertSpring0.add(testOperation(i, true, true));
-            resultsUpdateSpring0.add(testOperation(i, false, true));
-            resultsInsertSpring1.add(testOperation(i, true, false));
-            resultsUpdateSpring1.add(testOperation(i, false, false));
-            useSpring = false;
-            resultsInsertJdbc0.add(testOperation(i, true, true));
-            resultsUpdateJdbc0.add(testOperation(i, false, true));
+            resultsInsertNameParam.add(testOperation(i, true, TestType.NamedParameterJdbctemplate));
+            resultsUpdateNameParam.add(testOperation(i, false, TestType.NamedParameterJdbctemplate));
+            resultsInsertJdbcOperation.add(testOperation(i, true, TestType.JdbcOperation));
+            resultsUpdateJdbcOperation.add(testOperation(i, false, TestType.JdbcOperation));
+            resultsInsertPrepareStatment.add(testOperation(i, true, TestType.PrepareStatment));
+            resultsUpdatePrepareStatment.add(testOperation(i, false, TestType.PrepareStatment));
 
             dropTable();
         }
 
-        printResult("Test Spring Insert Using Params", resultsInsertSpring0);
-        printResult("Test Spring Update Using Params", resultsUpdateSpring0);
-        printResult("Test Spring Update Not Using Params", resultsInsertSpring1);
-        printResult("Test Spring Update Not Using Params", resultsUpdateSpring1);
-
-        printResult("Test JDBC Insert Using Params", resultsInsertJdbc0);
-        printResult("Test JDBC Update Using Params", resultsUpdateJdbc0);
+        printResult("Test NamedParameterJdbctemplate Insert", resultsInsertNameParam);
+        printResult("Test NamedParameterJdbctemplate Update", resultsUpdateNameParam);
+        printResult("Test JdbcOperation Insert", resultsInsertJdbcOperation);
+        printResult("Test JdbcOperation Update", resultsUpdateJdbcOperation);
+        printResult("Test PrepareStatment Insert", resultsInsertPrepareStatment);
+        printResult("Test PrepareStatment Update", resultsUpdatePrepareStatment);
     }
 
     private void printResult(String header, List<List<Long>> results) {
@@ -116,102 +123,154 @@ public class TestLageDomainObject {
         }
     }
 
-    private List<Long> testOperation(int fieldsCount, boolean create, boolean usingParams) throws SQLException {
+    private List<Long> testOperation(int fieldsCount, boolean create, TestType testType) throws SQLException, OperationNotSupportedException {
         List<Long> result = new ArrayList<Long>();
-        //в цикле выполняем insert c разным количеством полей с шагом 10
+        //в цикле выполняем insert c разным количеством полей с шагом QUERY_FIELDS_STEP
         for (int i = 0; i <= fieldsCount; i = i + QUERY_FIELDS_STEP) {
             long time = 0;
             for (int j = 0; j < ITERATIONS_COUNT; j++) {
                 if (create) {
-                    time += insert(i, usingParams);
+                    switch (testType) {
+                        case NamedParameterJdbctemplate:
+                            time += insertUsingNamedParameterJdbcTemplate(i);
+                            break;
+                        case PrepareStatment:
+                            time += insertUsingPrepareStatment(i);
+                            break;
+                        case JdbcOperation:
+                            time += insertUsingJdbcOperation(i);                            
+                            break;
+                        default:
+                            throw new OperationNotSupportedException();
+                    }                    
                 } else {
-                    time += update(i, usingParams);
+                    switch (testType) {
+                        case NamedParameterJdbctemplate:
+                            time += updateUsingNamedParameterJdbcTemplate(i);
+                            break;
+                        case PrepareStatment:
+                            time += updateUsingPrepareStatment(i);
+                            break;
+                        case JdbcOperation:
+                            time += updateUsingJdbcOperation(i);                            
+                            break;
+                        default:
+                            throw new OperationNotSupportedException();
+                    }                    
                 }
             }
             long workTime = time / ITERATIONS_COUNT;
             result.add(workTime);
-            System.out.println((create ? "Create " : "Update ") + (i) + "=" + workTime + " ms");
+            System.out.println(testType.toString() + " " + (create ? "Create " : "Update ") + (i) + "=" + workTime + " ms");
         }
         return result;
     }
 
-    private long update(int countFields, boolean usingParams) throws SQLException {
+    private long updateUsingNamedParameterJdbcTemplate(int countFields) throws SQLException {
         String query = "update test_lage_table set ";
         for (int i = 0; i < countFields; i++) {
             if (i > 0) {
                 query += ",";
             }
-            if (usingParams) {
-                if (useSpring) {
-                    query += "field" + i + "= :field" + i;
-                } else {
-                    query += "field" + i + "= ?";
-                }
-            } else {
-                query += "field" + i + "= null";
-            }
+            query += "field" + i + "= :field" + i;
         }
-        if (useSpring){
-            query += " where id = :id";
-        }else{
-            query += " where id = ?";
-        }
+        query += " where id = :id";
 
         Map<String, Object>[] parameters = new Map[1];
         parameters[0] = new HashMap<String, Object>();
 
-        if (usingParams) {
-            for (int i = 0; i < countFields; i++) {
-                parameters[0].put("field" + i, null);
-            }
+        for (int i = 0; i < countFields; i++) {
+            parameters[0].put("field" + i, null);
         }
 
         parameters[0].put("id", getRndId());
-        PreparedStatement ps = null;
-        if (!useSpring && countFields > 0){
-            ps = dataSource.getConnection().prepareStatement(query);
-            for (int i = 1; i < countFields + 1; i++) {
-                ps.setNull(i, Types.VARCHAR);
-            }
-            ps.setLong(countFields + 1, getNextId());
-        }
 
         long start = System.currentTimeMillis();
         if (countFields > 0) {
-            if (useSpring) {
-                jdbcTemplate.batchUpdate(query, parameters);
-            } else {
-                ps.execute();
-            }
+            jdbcTemplate.batchUpdate(query, parameters);
         }
         return System.currentTimeMillis() - start;
     }
 
-    private Object getRndId() {
+    private long updateUsingPrepareStatment(int countFields) throws SQLException {
+        String query = "update test_lage_table set ";
+        for (int i = 0; i < countFields; i++) {
+            if (i > 0) {
+                query += ",";
+            }
+            query += "field" + i + "= ?";
+        }
+        query += " where id = ?";
+
+        PreparedStatement ps = null;
+        if (countFields > 0){
+            ps = dataSource.getConnection().prepareStatement(query);
+            for (int i = 1; i < countFields + 1; i++) {
+                ps.setNull(i, Types.VARCHAR);
+            }
+            ps.setLong(countFields + 1, getRndId());
+        }
+
+        long start = System.currentTimeMillis();
+        if (countFields > 0) {
+            ps.execute();
+        }
+        return System.currentTimeMillis() - start;
+    }
+    
+    private long updateUsingJdbcOperation(int countFields) throws SQLException {
+        String query = "update test_lage_table set ";
+        for (int i = 0; i < countFields; i++) {
+            if (i > 0) {
+                query += ",";
+            }
+            query += "field" + i + "= ?";
+        }
+        query += " where id = ?";
+
+        List<Map<String, Object>> parameters = new ArrayList<>(1);
+        
+        
+        Map<String, Object> parameter = new HashMap<String, Object>();
+        Map<String, List<Integer>> parameterIndexMap = new HashMap<String, List<Integer>>();        
+
+        for (int i = 0; i < countFields; i++) {
+            parameter.put("field" + i, null);
+            List<Integer> index = new ArrayList<Integer>();
+            index.add(i+1);
+            parameterIndexMap.put("field" + i, index);
+        }
+
+        parameter.put("id", getRndId());
+        parameters.add(parameter);
+        
+        List<Integer> index = new ArrayList<Integer>();
+        index.add(countFields + 1);
+        parameterIndexMap.put("id", index);
+        
+        BatchPreparedStatementSetter batchPreparedStatementSetter = new BatchPreparedStatementSetter(parameterIndexMap);
+        
+        long start = System.currentTimeMillis();
+        if (countFields > 0) {
+            jdbcOperations.batchUpdate(query, parameters, 1, batchPreparedStatementSetter);
+        }
+        long result = System.currentTimeMillis() - start; 
+        return result;
+    }
+    
+    private Long getRndId() {
         return ids.get(rnd.nextInt(ids.size()));
     }
 
-    private long insert(int countFields, boolean usingParams) throws SQLException {
+    private long insertUsingNamedParameterJdbcTemplate(int countFields) throws SQLException {
         String query = "insert into test_lage_table (id";
         for (int i = 0; i < countFields; i++) {
             query += ", field" + i;
         }
         query += ") values (";
-        if (useSpring) {
-            query += ":id";
-        } else {
-            query += "?";
-        }
+        query += ":id";
         for (int i = 0; i < countFields; i++) {
-            if (usingParams) {
-                if (useSpring) {
-                    query += ", :field" + i;
-                } else {
-                    query += ", ?";
-                }
-            } else {
-                query += ", null";
-            }
+            query += ", :field" + i;
         }
         query += ")";
 
@@ -219,30 +278,78 @@ public class TestLageDomainObject {
         parameters[0] = new HashMap<String, Object>();
         parameters[0].put("id", getNextId());
 
-        if (usingParams) {
-            for (int i = 0; i < countFields; i++) {
-                parameters[0].put("field" + i, null);
-            }
-        }
-
-        PreparedStatement ps = null;
-        if (!useSpring) {
-            ps = dataSource.getConnection().prepareStatement(query);
-            ps.setLong(1, getNextId());
-            for (int i = 2; i < countFields + 2; i++) {
-                ps.setNull(i, Types.VARCHAR);
-            }
+        for (int i = 0; i < countFields; i++) {
+            parameters[0].put("field" + i, null);
         }
 
         long start = System.currentTimeMillis();
-        if (useSpring) {
-            jdbcTemplate.batchUpdate(query, parameters);
-        } else {
-            ps.execute();
-        }
+        jdbcTemplate.batchUpdate(query, parameters);
         return System.currentTimeMillis() - start;
     }
 
+    private long insertUsingPrepareStatment(int countFields) throws SQLException {
+        String query = "insert into test_lage_table (id";
+        for (int i = 0; i < countFields; i++) {
+            query += ", field" + i;
+        }
+        query += ") values (";
+        query += "?";
+        for (int i = 0; i < countFields; i++) {
+            query += ", ?";
+        }
+        query += ")";
+
+        PreparedStatement ps = null;
+        ps = dataSource.getConnection().prepareStatement(query);
+        ps.setLong(1, getNextId());
+        for (int i = 2; i < countFields + 2; i++) {
+            ps.setNull(i, Types.VARCHAR);
+        }
+
+        long start = System.currentTimeMillis();
+        ps.execute();
+        return System.currentTimeMillis() - start;
+    }
+    
+    private long insertUsingJdbcOperation(int countFields) throws SQLException {
+        String query = "insert into test_lage_table (id";
+        for (int i = 0; i < countFields; i++) {
+            query += ", field" + i;
+        }
+        query += ") values (";
+        query += "?";
+        for (int i = 0; i < countFields; i++) {
+            query += ", ?";
+        }
+        query += ")";
+
+        List<Map<String, Object>> parameters = new ArrayList<>(1);        
+        
+        Map<String, Object> parameter = new HashMap<String, Object>();
+        Map<String, List<Integer>> parameterIndexMap = new HashMap<String, List<Integer>>();        
+
+        for (int i = 0; i < countFields; i++) {
+            parameter.put("field" + i, null);
+            List<Integer> index = new ArrayList<Integer>();
+            index.add(i+2);
+            parameterIndexMap.put("field" + i, index);
+        }
+
+        parameter.put("id", getNextId());
+        parameters.add(parameter);
+        
+        List<Integer> index = new ArrayList<Integer>();
+        index.add(1);
+        parameterIndexMap.put("id", index);
+        
+        BatchPreparedStatementSetter batchPreparedStatementSetter = new BatchPreparedStatementSetter(parameterIndexMap);
+        
+        long start = System.currentTimeMillis();
+        jdbcOperations.batchUpdate(query, parameters, 1, batchPreparedStatementSetter);
+        return System.currentTimeMillis() - start;
+    }
+
+    
     private void createTable(int fieldsCount) {
         String query = "create table test_lage_table (";
         query += "id bigint NOT NULL,";
