28ac450f8c14fc123af9f3fb04c7223f08bf5899
rabdulkhairov <ravil200606@gmail.com>
2015-09-08 11:44:16 +0300

    CMFIVE-4040 Поддержка различных панелей навигации по разным URL

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/BaseUrlDispatcherServlet.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/BaseUrlDispatcherServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * @author Ravil Abdulkhairov
@@ -13,33 +12,11 @@ import java.io.PrintWriter;
  * @since 08.09.2015
  */
 public class BaseUrlDispatcherServlet extends HttpServlet {
+    private static final String BU_PAGE = "/BusinessUniverse.html";
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        // Set the response message's MIME type
-        response.setContentType("text/html;charset=UTF-8");
-        // Allocate a output writer to write the response message into the network socket
-        PrintWriter out = response.getWriter();
-
-        // Write the response message, in an HTML page
-        try {
-            out.println("<!DOCTYPE html>");
-            out.println("<html><head>");
-            out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
-            out.println("<title>Hello, World</title></head>");
-            out.println("<body>");
-            out.println("<h1>Hello, world!</h1>");  // says Hello
-            // Echo client's request information
-            out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
-            out.println("<p>Protocol: " + request.getProtocol() + "</p>");
-            out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
-            out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
-            // Generate a random number upon each request
-            out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();  // Always close the output writer
-        }
+        response.sendRedirect(request.getContextPath() + BU_PAGE);
     }
 }
