e09e5f4d4b00daab8c5429cdef4b2397cae32d54
rabdulkhairov <ravil200606@gmail.com>
2016-05-10 09:34:38 +0300

    CMFIVE-5250 Опечатка в форме создания Этапа Согласования.

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/util/WidgetConstants.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/util/WidgetConstants.java
@@ -6,7 +6,7 @@ package ru.intertrust.cm.core.gui.impl.server.util;
  * on 12.06.2014.
  */
 public class WidgetConstants {
-    public static final String REPRESENTATION_STUB = "Невозможно отобразить елемент";
+    public static final String REPRESENTATION_STUB = "Невозможно отобразить элемент";
     public static final String UNDEFINED_COLLECTION_NAME = "undefined";
     public static final int UNBOUNDED_LIMIT = 999999999;
     public static final String EMPTY_VALUE = "";
