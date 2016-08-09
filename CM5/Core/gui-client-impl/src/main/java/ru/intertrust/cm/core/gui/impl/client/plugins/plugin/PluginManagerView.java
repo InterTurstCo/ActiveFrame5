4d21dfe735c0115d99ba10634c0a72c99bedda6e
Andrei Lozovoy <andrei.lozovoy@gmail.com>
2016-08-09 13:55:07 +0300

    CMFIVE-6038. Привести в порядок интерфейс менеджера плагинов. Добавлены дополнительные столбцы. и возможность скачивать последний актуальный лог.

--- CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/plugins/plugin/PluginManagerView.java
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/plugins/plugin/PluginManagerView.java
@@ -1,8 +1,6 @@
 package ru.intertrust.cm.core.gui.impl.client.plugins.plugin;
 
-import com.google.gwt.cell.client.Cell;
-import com.google.gwt.cell.client.CheckboxCell;
-import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.cell.client.*;
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
@@ -10,6 +8,7 @@ import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.SimplePager;
@@ -20,6 +19,7 @@ import com.google.gwt.user.client.ui.*;
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.web.bindery.event.shared.EventBus;
 import ru.intertrust.cm.core.business.api.dto.Dto;
+import ru.intertrust.cm.core.business.api.dto.Id;
 import ru.intertrust.cm.core.business.api.plugin.PluginInfo;
 import ru.intertrust.cm.core.config.gui.form.widget.WidgetDisplayConfig;
 import ru.intertrust.cm.core.config.localization.LocalizationKeys;
@@ -29,22 +29,27 @@ import ru.intertrust.cm.core.gui.api.client.LocalizeUtil;
 import ru.intertrust.cm.core.gui.impl.client.ApplicationWindow;
 import ru.intertrust.cm.core.gui.impl.client.Plugin;
 import ru.intertrust.cm.core.gui.impl.client.PluginView;
+import ru.intertrust.cm.core.gui.impl.client.action.Action;
 import ru.intertrust.cm.core.gui.impl.client.form.widget.attachmentbox.AttachmentBoxWidget;
 import ru.intertrust.cm.core.gui.impl.client.plugins.collection.view.LabledCheckboxCell;
 import ru.intertrust.cm.core.gui.impl.client.themes.GlobalThemesManager;
 import ru.intertrust.cm.core.gui.impl.client.util.BusinessUniverseConstants;
 import ru.intertrust.cm.core.gui.model.Command;
+import ru.intertrust.cm.core.gui.model.action.DownloadAttachmentActionContext;
 import ru.intertrust.cm.core.gui.model.form.widget.AttachmentBoxState;
 import ru.intertrust.cm.core.gui.model.plugin.ExecutePluginData;
 import ru.intertrust.cm.core.gui.model.plugin.PluginInfoData;
 import ru.intertrust.cm.core.gui.model.plugin.UploadData;
 import ru.intertrust.cm.core.gui.rpc.api.BusinessUniverseServiceAsync;
 
+import java.util.Date;
+
 import static ru.intertrust.cm.core.config.localization.LocalizationKeys.EXECUTION_ACTION_ERROR_KEY;
 import static ru.intertrust.cm.core.gui.impl.client.util.BusinessUniverseConstants.EXECUTION_ACTION_ERROR;
 
 public class PluginManagerView extends PluginView {
 
+    public static final String DATE_TIME_PATTERN = "dd-mm-yyyy HH:mm:ss";
     private Panel mainPanel = new VerticalPanel();
     private AttachmentBoxWidget attachmentBox;
     private CellTable<PluginInfo> cellTable;
@@ -52,6 +57,7 @@ public class PluginManagerView extends PluginView {
     private SimplePager pager;
     private Button uploadButton;
     private Button executeButton;
+    private Button updateButton;
     private ListDataProvider<PluginInfo> dataProvider = new ListDataProvider<PluginInfo>();
 
     private PluginManagerParamDialogBox dialogBox;
@@ -281,6 +287,57 @@ public class PluginManagerView extends PluginView {
         cellTable.addColumn(descriptionColumn, "description");
         cellTable.setColumnWidth(descriptionColumn, 100, Unit.PCT);
 
+
+        Column<PluginInfo, Date> startPluginTimeColumn = new Column<PluginInfo, Date>(new DateCell(DateTimeFormat.getFormat(DATE_TIME_PATTERN))) {
+            @Override
+            public Date getValue(PluginInfo object) {
+                return object.getLastStart();
+            }
+        };
+
+        cellTable.addColumn(startPluginTimeColumn, "start plugin time");
+        cellTable.setColumnWidth(startPluginTimeColumn, 100, Unit.PCT);
+
+        Column<PluginInfo, Date> finishPluginTimeColumn = new Column<PluginInfo, Date>(new DateCell(DateTimeFormat.getFormat(DATE_TIME_PATTERN))) {
+            @Override
+            public Date getValue(PluginInfo object) {
+                return object.getLastFinish();
+            }
+        };
+        cellTable.addColumn(finishPluginTimeColumn, "finish plugin time");
+        cellTable.setColumnWidth(finishPluginTimeColumn, 100, Unit.PCT);
+
+
+        Column<PluginInfo, String> pluginStatusColumn = new Column<PluginInfo, String>(new TextCell()) {
+            @Override
+            public String getValue(PluginInfo object) {
+                return object.getStatus().toString();
+            }
+        };
+        cellTable.addColumn(pluginStatusColumn, "plugin status");
+        cellTable.setColumnWidth(pluginStatusColumn, 100, Unit.PCT);
+
+
+        Column<PluginInfo, String> downloadLogColumn = new Column<PluginInfo, String>(
+                new ButtonCell()) {
+            @Override
+            public String getValue(PluginInfo object) {
+                return "Скачать лог";
+            }
+
+            @Override
+            public void onBrowserEvent(Cell.Context context, Element elem, PluginInfo object, NativeEvent event) {
+
+                DownloadAttachmentActionContext logContext = new DownloadAttachmentActionContext();
+                logContext.setId(object.getLastResult());
+                final Action action = ComponentRegistry.instance.get("download.attachment.action");
+                action.setInitialContext(logContext);
+                action.perform();
+            }
+        };
+
+        cellTable.addColumn(downloadLogColumn, "last log");
+        cellTable.setColumnWidth(downloadLogColumn, 100, Unit.PCT);
     }
 
 
@@ -300,9 +357,25 @@ public class PluginManagerView extends PluginView {
                public void onClick(ClickEvent event) {
                    dialogBox.checkAndSetParamValue(dataProvider.getList());
                    dialogBox.showDialogBox();
+                   refreshPluginsModel();
                }
            }
         );
+
+        updateButton = new Button();
+        updateButton.setStylePrimaryName("btn-update");
+        updateButton.addStyleName(GlobalThemesManager.getCurrentTheme().commonCss().addDoBtn());
+        updateButton.addStyleName("add-btn-table-viewer");
+        updateButton.setTitle("Обновить");
+        toolbarPanel.add(updateButton);
+
+
+        updateButton.addClickHandler(new ClickHandler() {
+              @Override
+              public void onClick(ClickEvent event) {
+                  refreshPluginsModel();
+              }
+          });
 
     }
 
