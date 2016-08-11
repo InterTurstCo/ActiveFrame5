ab70563f101ad9776c3b0cbcc876beb40f8a874d
Alexander Bogatyrenko <alexander.bogatyr@gmail.com>
2016-08-11 15:54:52 +0300

    CMFIVE-5987 - минорные исправления

--- CM5/deployment-tool/src/main/java/ru/intertrust/cm/deployment/tool/config/AppConstants.java
+++ CM5/deployment-tool/src/main/java/ru/intertrust/cm/deployment/tool/config/AppConstants.java
@@ -12,6 +12,6 @@ public final class AppConstants {
 
     public static final String ARCHIVE_NAME_PATTERN = "%s-%s.zip";
     public static final String SERVER_URL_PATTERN = "http://%s:%s";
-    public static final String APP_URL_PATTERN = "http://%s:%s/%s";
+    public static final String APP_URL_PATTERN = "http://%s:%s%s";
     public static final String INIT_DATA_PROPERTY_NAME = "REPOSITORY_INIT_FOLDER";
 }
