b4c414becfaf978321253b5d8291cf2b0ed6af17
rabdulkhairov <ravil200606@gmail.com>
2016-09-21 09:12:51 +0300

    CMFIVE-792 Плагин, загружающий шаблоны отчётов - расширение функциональности

---
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/action/UninstallReportActionHandler.java
@@ -0,0 +1,38 @@
+package ru.intertrust.cm.core.gui.impl.server.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.intertrust.cm.core.business.api.CrudService;
+import ru.intertrust.cm.core.business.api.ReportServiceAdmin;
+import ru.intertrust.cm.core.business.api.dto.Id;
+import ru.intertrust.cm.core.gui.api.server.action.ActionHandler;
+import ru.intertrust.cm.core.gui.model.ComponentName;
+import ru.intertrust.cm.core.gui.model.action.ActionContext;
+import ru.intertrust.cm.core.gui.model.action.ActionData;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * Developer: Ravil Abdulkhairov
+ * Date: 20.09.2016
+ * Time: 14:34
+ * To change this template use File | Settings | File and Code Templates.
+ */
+@ComponentName("uninstall.report.action")
+public class UninstallReportActionHandler extends ActionHandler<ActionContext, ActionData> {
+
+    private static final String REPORT_NAME = "name";
+    @Autowired
+    ReportServiceAdmin reportServiceAdmin;
+
+
+    @Autowired
+    CrudService crudService;
+
+    @Override
+    public ActionData executeAction(ActionContext context) {
+        Id rootObject = context.getRootObjectId();
+        String reportName = crudService.find(rootObject).getString(REPORT_NAME);
+        reportServiceAdmin.undeploy(reportName);
+        return new ActionData();
+    }
+}
