7a643529e79f6bbf2ceeebe4d24d0abf9f9d555c
mlarin <larin@btlab.ru>
2017-01-27 10:26:50 +0300

    CMFIVE-7473 Заглушка сервиса глобальных настроек.

---
+++ CM5/Core/service-api/src/main/java/ru/intertrust/cm/core/business/api/GlobalServerSettingsService.java
@@ -0,0 +1,55 @@
+package ru.intertrust.cm.core.business.api;
+
+/**
+ * Сервис глобальных настроек сервера (кластера серверов). Получает значения из таблицы global_server_settings
+ * и если там нет из server.properties, и если нигде нет из параметра defaultValue
+ * @author larin
+ *
+ */
+public interface GlobalServerSettingsService {
+    
+    /**
+     * Получение строкового значения из глобальной конфигурации сервера 
+     * @param name
+     * @return
+     */
+    public String getString(String name);
+    
+    /**
+     * Получение строкового значения из глобальной конфигурации сервера, и значения по умолчанию если в конфигурации не найден
+     * @param name
+     * @param defaultValue
+     * @return
+     */
+    public String getString(String name, String defaultValue);
+    
+    /**
+     * Получение boolean значения из глобальной конфигурации сервера
+     * @param name
+     * @return
+     */
+    public Boolean getBoolean(String name);
+    
+    /**
+     * Получение boolean значения из глобальной конфигурации сервера, и значения по умолчанию если в конфигурации не найден
+     * @param name
+     * @param defaultValue
+     * @return
+     */
+    public Boolean getBoolean(String name, Boolean defaultValue);
+    
+    /**
+     * Получение числового значения из глобальной конфигурации сервера 
+     * @param name
+     * @return
+     */
+    public Long getLong(String name);
+    
+    /**
+     * Получение числового значения из глобальной конфигурации сервера, и значения по умолчанию если в конфигурации не найден
+     * @param name
+     * @param defaultValue
+     * @return
+     */
+    public Long getLong(String name, Long defaultValue);
+}
---
+++ CM5/Core/service-impl/src/main/java/ru/intertrust/cm/core/business/impl/GlobalServerSettingsServiceImpl.java
@@ -0,0 +1,43 @@
+package ru.intertrust.cm.core.business.impl;
+
+import ru.intertrust.cm.core.business.api.GlobalServerSettingsService;
+
+public class GlobalServerSettingsServiceImpl implements GlobalServerSettingsService {
+
+    @Override
+    public String getString(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getString(String name, String defaultValue) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean getBoolean(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean getBoolean(String name, Boolean defaultValue) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long getLong(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long getLong(String name, Long defaultValue) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
