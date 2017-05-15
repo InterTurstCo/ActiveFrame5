c25bfe6ae5d2a446b4db6b24400300e8f82798fb
Ravil <ravil200606@gmail.com>
2017-05-15 17:29:38 +0300

    CMFIVE-9253 Реализовать GUI для создания и замещения конфигураций

---
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/extension/DistroXmlHandler.java
@@ -0,0 +1,45 @@
+package ru.intertrust.cm.core.gui.impl.server.extension;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.intertrust.cm.core.config.ConfigurationExplorer;
+import ru.intertrust.cm.core.config.ConfigurationSerializer;
+import ru.intertrust.cm.core.config.base.TopLevelConfig;
+import ru.intertrust.cm.core.gui.api.server.widget.WidgetContext;
+import ru.intertrust.cm.core.gui.impl.server.widget.TextAreaHandler;
+import ru.intertrust.cm.core.gui.model.ComponentName;
+import ru.intertrust.cm.core.gui.model.form.widget.TextState;
+
+import java.util.List;
+
+/**
+ * Created by Ravil on 15.05.2017.
+ */
+@ComponentName("distro.xml.handler")
+public class DistroXmlHandler extends TextAreaHandler {
+
+    private static final String FIELD_NAME = "name";
+
+    @Autowired
+    private ConfigurationExplorer configurationExplorer;
+
+    @Autowired
+    private ConfigurationSerializer configurationSerializer;
+
+    private String tagName;
+
+    @Override
+    public TextState getInitialState(WidgetContext context) {
+        TextState tState = super.getInitialState(context);
+        if(context.getFormObjects().getRootDomainObject()!=null){
+            tagName = context.getFormObjects().getRootDomainObject().getString(FIELD_NAME);
+            List<TopLevelConfig> configs =  configurationExplorer.getDistributiveConfiguration().getConfigurationList();
+            for(TopLevelConfig c : configs){
+                if(c.getName().equals(tagName)){
+                    tState.setText(configurationSerializer.serializeConfiguration(c));
+                    break;
+                }
+            }
+        }
+        return tState;
+    }
+}
