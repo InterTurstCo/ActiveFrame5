3b3101e1e7490868dd804f6e344b8c672364cc91
Ravil <ravil200606@gmail.com>
2017-05-16 12:37:51 +0300

    CMFIVE-9253 Реализовать GUI для создания и замещения конфигураций

---
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/extension/DeleteActionVisibilityChecker.java
@@ -0,0 +1,19 @@
+package ru.intertrust.cm.core.gui.impl.server.extension;
+
+import ru.intertrust.cm.core.gui.api.server.action.ActionVisibilityChecker;
+import ru.intertrust.cm.core.gui.api.server.action.ActionVisibilityContext;
+import ru.intertrust.cm.core.gui.model.ComponentName;
+
+
+/**
+ * Created by Ravil on 16.05.2017.
+ */
+@ComponentName("extension.delete.visibility.checker")
+public class DeleteActionVisibilityChecker implements ActionVisibilityChecker {
+
+
+    @Override
+    public boolean isVisible(ActionVisibilityContext context) {
+       return true;
+    }
+}
