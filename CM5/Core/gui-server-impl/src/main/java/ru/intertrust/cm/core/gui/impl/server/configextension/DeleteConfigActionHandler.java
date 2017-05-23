1df124bf297ead73f14e7b792659c3cb935b619d
Ravil <ravil200606@gmail.com>
2017-05-23 15:03:20 +0300

    CMFIVE-9253 Реализовать GUI для создания и замещения конфигураций

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/configextension/DeleteConfigActionHandler.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/configextension/DeleteConfigActionHandler.java
@@ -2,10 +2,17 @@ package ru.intertrust.cm.core.gui.impl.server.configextension;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.intertrust.cm.core.business.api.ConfigurationControlService;
+import ru.intertrust.cm.core.business.api.dto.Id;
+import ru.intertrust.cm.core.config.ConfigurationException;
+import ru.intertrust.cm.core.config.SummaryConfigurationException;
 import ru.intertrust.cm.core.gui.impl.server.action.DeleteActionHandler;
 import ru.intertrust.cm.core.gui.model.ComponentName;
+import ru.intertrust.cm.core.gui.model.GuiException;
 import ru.intertrust.cm.core.gui.model.action.ActionContext;
 import ru.intertrust.cm.core.gui.model.action.DeleteActionData;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Ravil on 23.05.2017.
@@ -18,8 +25,12 @@ public class DeleteConfigActionHandler extends DeleteActionHandler {
     @Override
     public DeleteActionData executeAction(ActionContext context) {
         DeleteActionData result = new DeleteActionData();
-        result.setId(context.getRootObjectId());
-        configurationControlService.deleteNewExtensions(context.getRootObjectId());
+        try {
+            configurationControlService.deleteNewExtensions(Arrays.asList(context.getRootObjectId()));
+            result.setId(context.getRootObjectId());
+        } catch(ConfigurationException e){
+            throw new GuiException(e.getMessage());
+        }
         return result;
     }
 }
