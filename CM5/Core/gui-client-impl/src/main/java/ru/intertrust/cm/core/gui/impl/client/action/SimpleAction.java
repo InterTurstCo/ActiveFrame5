e71e78aa1345ed471cb8b8f9eff28c8bf238d9ef
Ravil <ravil200606@gmail.com>
2017-09-21 09:10:40 +0300

    CMFIVE-11346 Реализация РМ администратора: контроль задач iDocs

--- CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/action/SimpleAction.java
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/action/SimpleAction.java
@@ -1,7 +1,10 @@
 package ru.intertrust.cm.core.gui.impl.client.action;
 
+import ru.intertrust.cm.core.business.api.dto.Id;
 import ru.intertrust.cm.core.gui.api.client.Component;
 import ru.intertrust.cm.core.gui.impl.client.event.UpdateCollectionEvent;
+import ru.intertrust.cm.core.gui.impl.client.plugins.collection.CollectionPlugin;
+import ru.intertrust.cm.core.gui.impl.client.plugins.objectsurfer.DomainObjectSurferPlugin;
 import ru.intertrust.cm.core.gui.model.ComponentName;
 import ru.intertrust.cm.core.gui.model.action.ActionContext;
 import ru.intertrust.cm.core.gui.model.action.ActionData;
@@ -21,6 +24,17 @@ public class SimpleAction extends SimpleServerAction {
     protected ActionContext appendCurrentContext(ActionContext initialContext) {
         final SimpleActionContext context = (SimpleActionContext) initialContext;
         final IsDomainObjectEditor editor = (IsDomainObjectEditor) getPlugin();
+        if(editor instanceof DomainObjectSurferPlugin){
+            CollectionPlugin cPlugin = ((DomainObjectSurferPlugin)editor).getCollectionPlugin();
+            if(cPlugin.getChangedRowsState()!=null){
+                context.getObjectsIds().clear();
+                for(Id id : cPlugin.getChangedRowsState().keySet()){
+                    if(cPlugin.getChangedRowsState().get(id)){
+                        context.getObjectsIds().add(id);
+                    }
+                }
+            }
+        }
         context.setRootObjectId(editor.getRootDomainObject().getId());
         context.setMainFormState(editor.getFormState());
         context.setPluginState(editor.getFormPluginState());
