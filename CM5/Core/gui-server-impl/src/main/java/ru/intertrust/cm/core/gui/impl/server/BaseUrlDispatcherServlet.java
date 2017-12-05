1a22dbf18b0ddce3a73e7a7af4be6bd9f9eec63d
Ravil <ravil200606@gmail.com>
2017-12-05 16:35:38 +0300

    CMFIVE-6343 Неверное поведение навигации в браузере при различных URL

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/BaseUrlDispatcherServlet.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/BaseUrlDispatcherServlet.java
@@ -1,20 +1,19 @@
 package ru.intertrust.cm.core.gui.impl.server;
 
+
 import ru.intertrust.cm.core.business.api.ConfigurationService;
-import ru.intertrust.cm.core.config.BusinessUniverseConfig;
 
 import javax.ejb.EJB;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
+
 
 /**
  * Этот клас просто диспатчит нас на BusinessUniverse.html при любом URL вида
  * http://context_path/base_url/something Доступа к конфигурации мы тут не имеем так что просто
  * передаем через атрибут сессии URL в класс BusinessUniverse и там уже парсим имя приложения
+ *
  * @author Ravil Abdulkhairov
  * @version 1.0
  * @since 08.09.2015
@@ -22,12 +21,26 @@ import java.io.IOException;
 public class BaseUrlDispatcherServlet extends HttpServlet {
     private static final String BU_PAGE = "/BusinessUniverse.html";
 
-
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response)
             throws IOException, ServletException {
         HttpSession session = request.getSession(false);
-        session.setAttribute("uri", request.getRequestURI());
-        response.sendRedirect(request.getContextPath() + BU_PAGE +((request.getQueryString()!=null)?"?"+request.getQueryString():""));
+        if(session.getAttribute("uri")==null)
+            session.setAttribute("uri", request.getRequestURI());
+        //response.sendRedirect(request.getContextPath() + BU_PAGE +((request.getQueryString()!=null)?"?"+request.getQueryString():""));
+
+        if(!request.getRequestURL().toString().contains(".")){
+            request.getRequestDispatcher(BU_PAGE).forward(request,response);
+        }
+        else {
+            request.getRequestDispatcher(request.getRequestURI().substring(request.getRequestURI().
+                    lastIndexOf(request.getAttribute(VirtualAppFilter.APP_NAME).toString())+4)).forward(request,response);
+        }
+    }
+
+    @Override
+    public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws IOException, ServletException{
+            request.getRequestDispatcher(request.getRequestURI().substring(request.getRequestURI().
+                    lastIndexOf(request.getAttribute(VirtualAppFilter.APP_NAME).toString())+4)).forward(request,response);
     }
 }
---
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/VirtualAppFilter.java
@@ -0,0 +1,100 @@
+package ru.intertrust.cm.core.gui.impl.server;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import ru.intertrust.cm.core.business.api.ConfigurationService;
+import ru.intertrust.cm.core.config.BusinessUniverseConfig;
+
+import javax.ejb.EJB;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+
+/**
+ * Created by Ravil on 05.12.2017.
+ */
+public class VirtualAppFilter implements Filter {
+    private final static String METHOD_POST = "POST";
+    public final static String APP_NAME = "Application-Name";
+    @EJB
+    private ConfigurationService configurationService;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        BusinessUniverseConfig businessUniverseConfig = configurationService.getConfig(BusinessUniverseConfig.class,
+                BusinessUniverseConfig.NAME);
+        if(businessUniverseConfig.getBaseUrlConfig() != null && businessUniverseConfig.getBaseUrlConfig().getValue() != null){
+            servletRequest.setAttribute(APP_NAME, "/" + businessUniverseConfig.getBaseUrlConfig().getValue());
+        }
+        if (((HttpServletRequest) servletRequest).getMethod().equalsIgnoreCase(METHOD_POST)
+                && ((HttpServletRequest) servletRequest).getRequestURL().toString().contains("/" + businessUniverseConfig.getBaseUrlConfig().getValue())) {
+
+
+            XSSRequestWrapper wrappedRequest = new XSSRequestWrapper(
+                    (HttpServletRequest) servletRequest);
+            String body = IOUtils.toString(wrappedRequest.getReader());
+            wrappedRequest.resetInputStream(body.replace("/" + businessUniverseConfig.getBaseUrlConfig().getValue(), StringUtils.EMPTY).toString().getBytes("UTF-8"));
+            filterChain.doFilter(wrappedRequest, servletResponse);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    class XSSRequestWrapper extends HttpServletRequestWrapper {
+
+
+        private byte[] rawData;
+        private HttpServletRequest request;
+        private ResettableServletInputStream servletStream;
+
+        public XSSRequestWrapper(HttpServletRequest request) {
+            super(request);
+            this.request = request;
+            this.servletStream = new ResettableServletInputStream();
+        }
+
+
+        public void resetInputStream(byte[] newRawData) {
+            servletStream.stream = new ByteArrayInputStream(newRawData);
+        }
+
+        @Override
+        public ServletInputStream getInputStream() throws IOException {
+            if (rawData == null) {
+                rawData = IOUtils.toByteArray(this.request.getReader());
+                servletStream.stream = new ByteArrayInputStream(rawData);
+            }
+            return servletStream;
+        }
+
+        @Override
+        public BufferedReader getReader() throws IOException {
+            if (rawData == null) {
+                rawData = IOUtils.toByteArray(this.request.getReader());
+                servletStream.stream = new ByteArrayInputStream(rawData);
+            }
+            return new BufferedReader(new InputStreamReader(servletStream));
+        }
+
+        private class ResettableServletInputStream extends ServletInputStream {
+
+            private InputStream stream;
+
+            @Override
+            public int read() throws IOException {
+                return stream.read();
+            }
+        }
+    }
+}
