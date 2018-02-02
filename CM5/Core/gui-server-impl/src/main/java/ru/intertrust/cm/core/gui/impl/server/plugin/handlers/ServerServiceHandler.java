c09ac0dcb018506fcea40ee068027910b29ef30c
Ravil <ravil200606@gmail.com>
2018-02-02 10:24:52 +0300

    CMFIVE-15512 SD: АРМ РВЗ. Некорректное поведение при подписание ЭП - рецидив

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/plugin/handlers/ServerServiceHandler.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/plugin/handlers/ServerServiceHandler.java
@@ -2,6 +2,8 @@ package ru.intertrust.cm.core.gui.impl.server.plugin.handlers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.intertrust.cm.core.business.api.PersonService;
 import ru.intertrust.cm.core.business.api.dto.Dto;
 import ru.intertrust.cm.core.gui.api.server.plugin.PluginHandler;
 import ru.intertrust.cm.core.gui.model.ComponentName;
@@ -13,11 +15,15 @@ import ru.intertrust.cm.core.gui.model.plugin.SomePluginData;
  */
 @ComponentName("ServerService.plugin")
 public class ServerServiceHandler extends PluginHandler {
+
+    @Autowired
+    PersonService personService;
+
     private static final Logger logger = LoggerFactory.getLogger(ServerServiceHandler.class);
 
     public PluginData writeLog(Dto object) {
         SomePluginData dto = (SomePluginData) object;
-        logger.info(dto.getText());
+        logger.info(personService.getCurrentPersonUid()+" " +dto.getText());
         return new SomePluginData();
     }
 }
