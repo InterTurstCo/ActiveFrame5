88ad37fc2420837bc3adc610b50e74ef55f68140
mlarin <larin@btlab.ru>
2018-08-15 14:04:00 +0300

    CMFIVE-21083 Автоматическая миграция поля типа timelessDate на date-time

--- CM5/Core/dao-impl/src/main/java/ru/intertrust/cm/core/dao/impl/FieldConfigChangeHandlerImpl.java
+++ CM5/Core/dao-impl/src/main/java/ru/intertrust/cm/core/dao/impl/FieldConfigChangeHandlerImpl.java
@@ -133,7 +133,7 @@ public class FieldConfigChangeHandlerImpl implements FieldConfigChangeHandler {
         ColumnInfo columnInfo = schemaCache.getColumnInfo(domainObjectTypeConfig, newFieldConfig);
 
         if (!newFieldConfig.getClass().equals(oldFieldConfig.getClass()) &&
-                !dataStructureDao.getSqlType(newFieldConfig).startsWith(columnInfo.getDataType()) &&
+                !columnInfo.getDataType().startsWith(dataStructureDao.getSqlType(newFieldConfig)) &&
                 !(TextFieldConfig.class.equals(newFieldConfig.getClass()) &&
                         StringFieldConfig.class.equals(oldFieldConfig.getClass()))) {
             throw new ConfigurationException("Configuration loading aborted: cannot change field type of " +
