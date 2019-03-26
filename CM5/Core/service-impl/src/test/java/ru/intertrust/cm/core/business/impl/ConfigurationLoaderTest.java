53607fe990c50e8f28af9a5e42838877edb8390b
N <natkurdova@mail.ru>
2019-03-26 18:32:42 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 16a4a8ae18ad5289139b277e7b3081bc6b655188.

--- CM5/Core/service-impl/src/test/java/ru/intertrust/cm/core/business/impl/ConfigurationLoaderTest.java
+++ CM5/Core/service-impl/src/test/java/ru/intertrust/cm/core/business/impl/ConfigurationLoaderTest.java
@@ -1,17 +1,14 @@
 package ru.intertrust.cm.core.business.impl;
 
-import static org.mockito.Mockito.verify;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import ru.intertrust.cm.core.business.api.ConfigurationLoadService;
 import ru.intertrust.cm.core.config.DomainObjectLogicalValidator;
-import ru.intertrust.cm.core.util.SpringApplicationContext;
+
+import static org.mockito.Mockito.verify;
 
 /**
  * @author vmatsukevich
@@ -22,19 +19,12 @@ import ru.intertrust.cm.core.util.SpringApplicationContext;
 public class ConfigurationLoaderTest {
 
     @InjectMocks
-    private ConfigurationLoader configurationLoader;
+    private ConfigurationLoader configurationLoader = new ConfigurationLoader();
     @Mock
     private ConfigurationLoadService configurationLoadService;
     @Mock
-    private DomainObjectLogicalValidator logicalValidator;
-    @Mock
-    private SpringApplicationContext applicationContext;    
+    DomainObjectLogicalValidator logicalValidator;
 
-    @Before
-    public void init() {
-        new SpringApplicationContext().setApplicationContext(null);
-    }
-    
     @Test
     public void testLoad() throws Exception {
         configurationLoader.load();
