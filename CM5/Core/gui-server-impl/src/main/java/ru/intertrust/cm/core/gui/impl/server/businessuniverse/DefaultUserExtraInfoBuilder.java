8915e4a9dd79ade4986ab1f4213f1652dd1793ee
Ravil <ravil200606@gmail.com>
2019-04-22 11:27:46 +0300

    CMFIVE-30381 Отображение подписи под ФИО в шапке

--- CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/businessuniverse/DefaultUserExtraInfoBuilder.java
+++ CM5/Core/gui-server-impl/src/main/java/ru/intertrust/cm/core/gui/impl/server/businessuniverse/DefaultUserExtraInfoBuilder.java
@@ -19,34 +19,48 @@ import java.util.List;
  */
 @ComponentName("default.user.extra.info.builder")
 public class DefaultUserExtraInfoBuilder implements UserExtraInfoBuilder {
-    @Autowired
-    private CurrentUserAccessor currentUserAccessor;
+  @Autowired
+  private CurrentUserAccessor currentUserAccessor;
 
-    @Autowired
-    private PersonManagementService personManagementService;
+  @Autowired
+  private PersonManagementService personManagementService;
 
-    @Override
-    public PlainTextUserExtraInfo getUserExtraInfo() {
-        final List<DomainObject> groups = personManagementService.getPersonGroups(currentUserAccessor.getCurrentUserId());
-        if (groups == null || groups.isEmpty()) {
-            return new PlainTextUserExtraInfo("");
-        }
-        String defaultGroup = "";
-        for (DomainObject groupDo : groups) {
-            final String groupName = groupDo.getString("group_name");
-            if (groupName == null || groupName.isEmpty()) {
-                continue;
-            }
-            if (groupName.equals("Superusers")) {
-                return new PlainTextUserExtraInfo(MessageResourceProvider.getMessage("Superuser", GuiContext.getUserLocale(), null));
-            } else if (groupName.equals("Administrators") || groupName.equals("Administrators_Read_Only")) {
-                return new PlainTextUserExtraInfo(MessageResourceProvider.getMessage("Administrator", GuiContext.getUserLocale(), null));
-            }
-            if (groupName.equals("AllPersons") || groupName.equals("Person")) { // system groups, ignore them
-                continue;
-            }
-            defaultGroup = groupName;
-        }
-        return new PlainTextUserExtraInfo(defaultGroup);
+  @Override
+  public PlainTextUserExtraInfo getUserExtraInfo() {
+    Boolean isSuperuser = false;
+    Boolean isAdministrators = false;
+    Boolean isInfoSecAuditor = false;
+    final List<DomainObject> groups = personManagementService.getPersonGroups(currentUserAccessor.getCurrentUserId());
+    if (groups == null || groups.isEmpty()) {
+      return new PlainTextUserExtraInfo("");
     }
+    String defaultGroup = "";
+    for (DomainObject groupDo : groups) {
+      final String groupName = groupDo.getString("group_name");
+      if (groupName == null || groupName.isEmpty()) {
+        continue;
+      }
+      if (groupName.equals("Superusers")) {
+        isSuperuser = true;
+      } else if (groupName.equals("InfoSecAuditor")) {
+        isInfoSecAuditor = true;
+      } else if (groupName.equals("Administrators") || groupName.equals("Administrators_Read_Only")) {
+        isAdministrators = true;
+      }
+      if (groupName.equals("AllPersons") || groupName.equals("Person")) { // system groups, ignore them
+        continue;
+      }
+      defaultGroup = groupName;
+    }
+    if((isSuperuser && !isInfoSecAuditor) || (isSuperuser && isInfoSecAuditor)){
+      return new PlainTextUserExtraInfo(MessageResourceProvider.getMessage("Superuser", GuiContext.getUserLocale(), null));
+    }
+    if(!isSuperuser && isInfoSecAuditor){
+      return new PlainTextUserExtraInfo(MessageResourceProvider.getMessage("InfoSecAuditor", GuiContext.getUserLocale(), null));
+    }
+    if(!isSuperuser && !isInfoSecAuditor && isAdministrators){
+      return new PlainTextUserExtraInfo(MessageResourceProvider.getMessage("Administrator", GuiContext.getUserLocale(), null));
+    }
+    return new PlainTextUserExtraInfo(defaultGroup);
+  }
 }
