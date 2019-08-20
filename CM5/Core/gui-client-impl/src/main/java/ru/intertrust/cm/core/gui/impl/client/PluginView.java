58c7f6c76915c5c93dbeb235604dc2254003590e
Ravil <ravil200606@gmail.com>
2019-08-20 16:52:20 +0300

    CMFIVE-33379 CM6.2.0.General. Платформа AF5. Архитектура GUI. Контекстное вычисление полей на форме. Разработка

--- CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/PluginView.java
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/PluginView.java
@@ -515,8 +515,8 @@ public abstract class PluginView implements IsWidget {
      */
     @Override
     public void onEventReceived(WidgetBroadcastEvent e) {
-      if ((getActionToolBar().hashCode()==e.getInitiatorToolBarHashCode()
-          && subscribedOn(e.getWidgetId()) && e.getWidgetId()!=null) || (itemConfig.getEventsTypeConfig() != null && e.getBroadcast())) {
+      if ((getActionToolBar().hashCode()==e.getInitiatorToolBarHashCode() && e.getWidgetId()!=null
+          && subscribedOn(e.getWidgetId())) || (itemConfig.getEventsTypeConfig() != null && e.getBroadcast())) {
         try {
           if (itemConfig.getRulesTypeConfig().getHideRulesTypeConfig() != null) {
             // Если правил сокрытия несколько то их суммарный результат должен быть true
