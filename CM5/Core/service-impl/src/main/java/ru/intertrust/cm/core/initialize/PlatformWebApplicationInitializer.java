c6385d265196013fd86a4325ed8b7eded3176589
mlarin <larin@btlab.ru>
2020-02-21 11:19:01 +0300

    CMFIVE-43710 Исправлен деплой платформы

--- CM5/Core/service-impl/src/main/java/ru/intertrust/cm/core/initialize/PlatformWebApplicationInitializer.java
+++ CM5/Core/service-impl/src/main/java/ru/intertrust/cm/core/initialize/PlatformWebApplicationInitializer.java
@@ -7,8 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.FatalBeanException;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.intertrust.cm.core.business.impl.GloballyLockableInitializer;
 import ru.intertrust.cm.core.util.SingletonBeanFactoryLocator;
 
@@ -36,6 +41,18 @@ public abstract class PlatformWebApplicationInitializer implements WebApplicatio
             logger.info("Start app initialize");
 
             onInitContext(servletContext, beanFactory);
+
+            // Проверить не добавлен ли уже листенер в servletContext при вызове onInitContext
+            WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+            if (webApplicationContext == null){
+                servletContext.setInitParameter(ContextLoader.CONFIG_LOCATION_PARAM, "META-INF/applicationContext.xml");
+                servletContext.addListener(new ContextLoaderListener(){
+                    @Override
+                    protected ApplicationContext loadParentContext(ServletContext servletContext) {
+                        return (ApplicationContext)beanFactory;
+                    }
+                });
+            }
 
             logger.info("Finish app initialize");
             globallyLockableInitializer.finish();
