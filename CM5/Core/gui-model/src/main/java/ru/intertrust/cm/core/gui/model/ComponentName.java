70bf992ab4b7606e964e1c871a685f2bbdc2f6df
mlarin <larin@btlab.ru>
2020-07-17 11:54:11 +0300

    CMFIVE-45458 Проблема с открытием REQUIRES_NEW транзакции

--- CM5/Core/gui-model/src/main/java/ru/intertrust/cm/core/gui/model/ComponentName.java
+++ CM5/Core/gui-model/src/main/java/ru/intertrust/cm/core/gui/model/ComponentName.java
@@ -1,11 +1,16 @@
 package ru.intertrust.cm.core.gui.model;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 /**
  * Аннотация, определяющая имя компонента GUI
  * @author Denis Mitavskiy
  *         Date: 22.07.13
  *         Time: 17:30
  */
+@Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(value = { java.lang.annotation.ElementType.TYPE })
 public @interface ComponentName {
     String value();
 }
