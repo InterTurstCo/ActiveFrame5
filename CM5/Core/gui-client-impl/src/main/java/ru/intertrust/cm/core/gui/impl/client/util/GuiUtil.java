c8c21eeb8169156027df093c17dae95f4d96630d
Myskin Sergey <myskin.sergey@gmail.com>
2020-07-20 13:01:16 +0300

    CMFIVE-46360 : В IE таблица с плагинами в клиенте администратора не помещается в экран

--- CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/util/GuiUtil.java
+++ CM5/Core/gui-client-impl/src/main/java/ru/intertrust/cm/core/gui/impl/client/util/GuiUtil.java
@@ -259,6 +259,32 @@ public final class GuiUtil {
         return navigator.userAgent;
     }-*/;
 
+    /**
+     * Проверяет является ли текущий браузер интернет эксплорером.
+     *
+     * @return true - является; false - не является.
+     */
+    public static native boolean isIE() /*-{
+        function check_browser() {
+            var ua = navigator.userAgent, tem,
+                M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+            if (/trident/i.test(M[1])) {
+                tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+                return {name: 'IE', version: (tem[1] || '')};
+            }
+            if (M[1] === 'Chrome') {
+                tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+                if (tem != null) return {name: tem[1].replace('OPR', 'Opera'), version: tem[2]};
+            }
+            M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+            if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+            return {name: M[0], version: M[1]};
+        }
+
+        var browserSpecs = check_browser();
+        return (browserSpecs.name === 'IE' || browserSpecs.name === 'MSIE');
+    }-*/;
+
     public static String getModalHeight(String domainObjectType, LinkedFormMappingConfig linkedFormMappingConfig,
                                         LinkedFormConfig lowPriorityLinkedFormConfig) {
         LinkedFormConfig linkedFormConfig = null;
