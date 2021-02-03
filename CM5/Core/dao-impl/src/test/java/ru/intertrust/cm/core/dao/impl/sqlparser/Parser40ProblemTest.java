f8560a930831e70ee5632ccf5a661653d2d23be0
mlarin <larin@btlab.ru>
2021-02-03 11:41:37 +0300

    CMSEVEN-3638 Тест парсера 4.1

--- CM5/Core/dao-impl/src/test/java/ru/intertrust/cm/core/dao/impl/sqlparser/Parser40ProblemTest.java
+++ CM5/Core/dao-impl/src/test/java/ru/intertrust/cm/core/dao/impl/sqlparser/Parser40ProblemTest.java
@@ -1,13 +1,14 @@
 package ru.intertrust.cm.core.dao.impl.sqlparser;
 
 import net.sf.jsqlparser.statement.select.Select;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class Parser40ProblemTest {
     @Test
-    public void testIgnoreValuesLists() {
+    public void testUnionWithBracket() {
         String query = "SELECT * FROM " +
                 "  ( " +
                 "    (SELECT A FROM tbl) " +
@@ -20,8 +21,27 @@ public class Parser40ProblemTest {
         assertEquals(select.toString(), query);
     }
 
+    @Test
+    @Ignore("Бага в парсере 4.1-RC1")
+    public void testUnionWithOrderAndLimit() {
+        String query = "SELECT id FROM table1 " +
+                "UNION " +
+                "SELECT id FROM table2 " +
+                "ORDER BY id ASC LIMIT 55";
+        query = formatToCompare(query);
+        SqlQueryParser parser = new SqlQueryParser(query);
+        Select select = parser.getSelectStatement();
+        assertEquals(select.toString(), query);
+    }
+
     private String formatToCompare(String sql){
         String result = sql;
+        while (result.indexOf("\t") > 0){
+            result = result.replace("\t", " ");
+        }
+        while (result.indexOf("\n") > 0){
+            result = result.replace("\n", " ");
+        }
         while (result.indexOf("  ") > 0){
             result = result.replace("  ", " ");
         }
